33c417fd6e9419dc4d4ec603acbd1342a4b0c331
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-09-09 21:03:38 +0200

    added generators/license.js

---
+++ generators/license.js
@@ -0,0 +1,7 @@
+// ==========================================================================
+// Project:   Simple Auth - Simple Authentication for Ember.js
+// Copyright: ©2013 Marco Otte-Witte and contributors
+//            ember.js is ©2011-2013 Tilde Inc.
+// License:   Licensed under MIT license
+//            See https://raw.github.com/simplabs/simple-auth/master/LICENSE
+// ==========================================================================
