a6c2ef399afec3c99a29f51c65619fb3e870cad2
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-09-09 21:04:11 +0200

    WIP: tests

---
+++ packages/simple-auth/test/session_test.js
@@ -0,0 +1,7 @@
+var get = Ember.get, set = Ember.set, _$;
+
+module('SimpleAuth.Session');
+
+test('exists', function() {
+  ok(Ember.Object.detect(SimpleAuth.Session), 'SimpleAuth.Session is an Ember.Object');
+});
