a667a697f8f8b0521a83effc56009815aeacf1eb
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-09-09 20:46:08 +0200

    put actions in actions: hash

--- packages/simple-auth/lib/login_controller.js
+++ packages/simple-auth/lib/login_controller.js
@@ -1,20 +1,22 @@
 App.LoginController = Ember.Controller.extend({
-  submit: function() {
-    var self = this;
-    var data = this.getProperties('identification', 'password');
-    if (!Ember.isEmpty(data.identification) && !Ember.isEmpty(data.password)) {
-      var postData = { session: { identification: data.identification, password: data.password } };
-      $.post(EmberAuthSimple.baseUrl + '/session', postData).then(function(response) {
-        var sessionData = (response.session || {})
-        session.set('authToken', sessionData.auth_token);
-        var attemptedTransition = session.get('attemptedTransition');
-        if (attemptedTransition) {
-          attemptedTransition.retry();
-          session.set('attemptedTransition', null);
-        } else {
-          self.transitionToRoute('index');
-        }
-      });
+  actions: {
+    login: function() {
+      var self = this;
+      var data = this.getProperties('identification', 'password');
+      if (!Ember.isEmpty(data.identification) && !Ember.isEmpty(data.password)) {
+        var postData = { session: { identification: data.identification, password: data.password } };
+        $.post(EmberAuthSimple.baseUrl + '/session', postData).then(function(response) {
+          var sessionData = (response.session || {})
+          session.set('authToken', sessionData.auth_token);
+          var attemptedTransition = session.get('attemptedTransition');
+          if (attemptedTransition) {
+            attemptedTransition.retry();
+            session.set('attemptedTransition', null);
+          } else {
+            self.transitionToRoute('index');
+          }
+        });
+      }
     }
   }
 });
