43c663e0ed06ef13a9c3b1fcf7de51435a1985fd
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-09-09 20:58:05 +0200

    don't route automatically

--- packages/simple-auth/lib/main.js
+++ packages/simple-auth/lib/main.js
@@ -5,10 +5,6 @@ require('simple-auth/logout_route');
 SimpleAuth = Ember.Object.extend({
   setup: function(app, options) {
     this.baseUrl = options.baseUrl || '';
-    app.Router.map(function() {
-      this.route('login');
-      this.route('logout');
-    });
     var session = SimpleAuth.Session.create();
     app.register('simple_auth:session', session, { instantiate: false, singleton: true });
     $.each(['model', 'controller', 'view', 'route'], function(i, component) {
