a3a699bbc96cf84ce879267a722146520ccb1092
Marco Otte-Witte <mottewitte@rim.com>
2013-09-10 12:59:44 +0200

    completed tests for Ember.SimpleAuth.Session

--- packages/ember-simple-auth/tests/session_test.js
+++ packages/ember-simple-auth/tests/session_test.js
@@ -6,6 +6,14 @@ test('exists', function() {
   ok(Ember.Object.detect(Ember.SimpleAuth.Session), 'Ember.SimpleAuth.Session is an Ember.Object');
 });
 
+test('reads the authToken from sessionStorage when it is initialized', function() {
+  var token = Math.random().toString(36);
+  sessionStorage.authToken = token;
+  var session = Ember.SimpleAuth.Session.create();
+
+  ok(session.get('authToken') === token, 'Ember.SimpleAuth.Session reads the authToken from sessionStorage when initialized');
+});
+
 test('persists the authToken to the sessionStorage when it changes', function() {
   var session = Ember.SimpleAuth.Session.create();
   var token = Math.random().toString(36);
