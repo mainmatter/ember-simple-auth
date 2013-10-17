b63f4ce882850662e58ecc3cfc999f1f1a1adf8b
Marco Otte-Witte <mottewitte@rim.com>
2013-10-17 18:25:15 +0200

    cleanup

--- packages/ember-simple-auth/tests/mixins/application_route_mixin_test.js
+++ packages/ember-simple-auth/tests/mixins/application_route_mixin_test.js
@@ -19,7 +19,7 @@ var ajaxMock = function(url, options) {
   };
 };
 
-module('Ember.SimpleAuth.LogoutActionableMixin', {
+module('Ember.SimpleAuth.ApplicationRouteMixin', {
   originalAjax: Ember.$.ajax,
   setup: function() {
     Ember.SimpleAuth.serverSessionRoute = '/session/route';
@@ -35,20 +35,20 @@ module('Ember.SimpleAuth.LogoutActionableMixin', {
 test('sends a DELETE request to the correct route on logout', function() {
   testRoute._actions['logout'].apply(testRoute);
 
-  equal(ajaxRequestUrl, '/session/route', 'Ember.SimpleAuth.LogoutRouteMixin sends a request to the correct route before model.');
-  equal(ajaxRequestOptions.type, 'DELETE', 'Ember.SimpleAuth.LogoutRouteMixin sends a DELETE request before model.');
+  equal(ajaxRequestUrl, '/session/route', 'Ember.SimpleAuth.ApplicationRouteMixin sends a request to the correct route on logout.');
+  equal(ajaxRequestOptions.type, 'DELETE', 'Ember.SimpleAuth.ApplicationRouteMixin a DELETE request on logout.');
 });
 
 test('destroys the current session on logout', function() {
   testRoute.set('session.authToken', 'some token');
   testRoute._actions['logout'].apply(testRoute);
 
-  equal(testRoute.get('session.isAuthenticated'), false, 'Ember.SimpleAuth.LogoutRouteMixin destroy the current session before model.');
+  equal(testRoute.get('session.isAuthenticated'), false, 'Ember.SimpleAuth.ApplicationRouteMixin destroy the current session on logout.');
 });
 
 test('redirects to the correct route on logout', function() {
   Ember.SimpleAuth.routeAfterLogout = 'some.route';
   testRoute._actions['logout'].apply(testRoute);
 
-  equal(testRoute.route, 'some.route', 'Ember.SimpleAuth.LogoutRouteMixin redirects to the correct route before model.');
+  equal(testRoute.route, 'some.route', 'Ember.SimpleAuth.ApplicationRouteMixin redirects to the correct route on logout.');
 });
