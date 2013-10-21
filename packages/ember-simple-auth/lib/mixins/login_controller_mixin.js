92e8cc46198827df6d79f460b066869acce53b2a
Marco Otte-Witte <mottewitte@rim.com>
2013-10-21 18:07:04 +0200

    automatically refresh tokens

--- packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
+++ packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
@@ -1,4 +1,24 @@
 Ember.SimpleAuth.LoginControllerMixin = Ember.Mixin.create({
+  refreshToken: function(refreshToken) {
+    var self     = this;
+    var postData = ['grant_type=refresh_token', 'refresh_token=' + refreshToken].join('&');
+    Ember.$.ajax(Ember.SimpleAuth.serverTokenRoute, {
+      type:        'POST',
+      data:        postData,
+      contentType: 'application/x-www-form-urlencoded'
+    }).then(function(response) {
+      self.get('session').setup(response);
+      self.scheduleTokenRefresh(response);
+    });
+  },
+  scheduleTokenRefresh: function(response) {
+    response            = response || {};
+    var tokenExpiration = response.expires_in || 0;
+    if (!Ember.isEmpty(response.refresh_token) && tokenExpiration - 10 > 0) {
+      Ember.run.cancel(this.refreshTokenLater);
+      this.refreshTokenLater = Ember.run.later(this, response.refresh_token, tokenExpiration - 10);
+    }
+  },
   actions: {
     login: function() {
       var self = this;
@@ -11,6 +31,7 @@ Ember.SimpleAuth.LoginControllerMixin = Ember.Mixin.create({
           contentType: 'application/x-www-form-urlencoded'
         }).then(function(response) {
           self.get('session').setup(response);
+          self.scheduleTokenRefresh(response);
           self.send('loginSucceeded');
         }, function(xhr, status, error) {
           self.send('loginFailed', xhr, status, error);
--- packages/ember-simple-auth/tests/mixins/login_controller_mixin_test.js
+++ packages/ember-simple-auth/tests/mixins/login_controller_mixin_test.js
@@ -20,8 +20,12 @@ var AjaxMock = Ember.Object.extend({
     this.requestOptions = options;
     return {
       then: function(success, fail) {
-        success(self.response);
-        fail('xhr', 'status', 'error');
+        if (!!success) {
+          success(self.response);
+        }
+        if (!!fail) {
+          fail('xhr', 'status', 'error');
+        }
       }
     };
   }
@@ -39,6 +43,7 @@ module('Ember.SimpleAuth.LoginControllerMixin', {
   },
   teardown: function() {
     Ember.$.ajax = this.originalAjax;
+    Ember.run.cancel(testController.refreshTokenLater);
   }
 });
 
@@ -48,7 +53,7 @@ test('sends a POST request to the server token route on login', function() {
   equal(ajaxMock.requestUrl, '/token/route', 'Ember.SimpleAuth.LoginControllerMixin sends a request to the serverTokenRoute on login.');
   equal(ajaxMock.requestOptions.type, 'POST', 'Ember.SimpleAuth.LoginControllerMixin sends a POST request on login.');
   equal(ajaxMock.requestOptions.data, 'grant_type=password&username=identification&password=password', 'Ember.SimpleAuth.LoginControllerMixin sends a request with the correct data on login.');
-  equal(ajaxMock.requestOptions.contentType, 'application/x-www-form-urlencoded', 'Ember.SimpleAuth.LoginControllerMixin sends a request with the content type "application/json" on login.');
+  equal(ajaxMock.requestOptions.contentType, 'application/x-www-form-urlencoded', 'Ember.SimpleAuth.LoginControllerMixin sends a request with the content type "application/x-www-form-urlencoded" on login.');
 });
 
 test('does not send a request on login when identification or password are empty', function() {
@@ -85,4 +90,38 @@ test('invokes the login failed action with the callback arguments', function() {
 
   ok(testController.invokedLoginFailed, 'Ember.SimpleAuth.LoginControllerMixin invokes the loginFailed action when the request fails.');
   deepEqual(testController.loginFailedArguments, ['xhr', 'status', 'error'], 'Ember.SimpleAuth.LoginControllerMixin invokes the loginFailed action with the callback arguments when the request fails.');
+});
+
+test('schedules a token refresh on login', function() {
+  ajaxMock.response = { access_token: 'authToken', expires_in: 100 };
+  testController.send('login');
+
+  equal(testController.refreshTokenLater, undefined, 'Ember.SimpleAuth.LoginControllerMixin does not schedule a token refresh when the server reponse does not contain a refresh_token on login.');
+
+  ajaxMock.response = { access_token: 'authToken', refresh_token: 'refresh_token' };
+  testController.send('login');
+
+  equal(testController.refreshTokenLater, undefined, 'Ember.SimpleAuth.LoginControllerMixin does not schedule a token refresh when the server reponse does not contain a expires_in on login.');
+
+  ajaxMock.response = { access_token: 'authToken', refresh_token: 'refresh_token', expires_in: 10 };
+  testController.send('login');
+
+  equal(testController.refreshTokenLater, undefined, 'Ember.SimpleAuth.LoginControllerMixin does not schedule a token refresh when the server reponse contains a expires_in less or equal 10 on login.');
+
+  ajaxMock.response = { access_token: 'authToken', refresh_token: 'refresh_token', expires_in: 100 };
+  testController.send('login');
+
+  notEqual(testController.refreshTokenLater, undefined, 'Ember.SimpleAuth.LoginControllerMixin schedules a token refresh when the server reponse contains a refresh_token and a expires_in on login.');
+});
+
+test('refreshes the token', function() {
+  ajaxMock.response = { access_token: 'authToken', refresh_token: 'refresh_token', expires_in: 100 };
+  testController.send('login');
+  testController.refreshToken('refresh_token');
+
+  equal(ajaxMock.requestUrl, '/token/route', 'Ember.SimpleAuth.LoginControllerMixin sends a request to the serverTokenRoute to refresh the token.');
+  equal(ajaxMock.requestOptions.type, 'POST', 'Ember.SimpleAuth.LoginControllerMixin sends a POST request to refresh the token.');
+  equal(ajaxMock.requestOptions.data, 'grant_type=refresh_token&refresh_token=refresh_token', 'Ember.SimpleAuth.LoginControllerMixin sends a request with the correct data to refresh the token.');
+  equal(ajaxMock.requestOptions.contentType, 'application/x-www-form-urlencoded', 'Ember.SimpleAuth.LoginControllerMixin sends a request with the content type "application/x-www-form-urlencoded" to refresh the token.');
+  notEqual(testController.refreshTokenLater, undefined, 'Ember.SimpleAuth.LoginControllerMixin schedules another token refresh when the server reponse contains a refresh_token and a expires_in on token refresh.');
 });
