75fe3bad4d7ccb4e885e375b62f9c1b09be3518c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-10-28 19:32:52 +0100

    docs for Ember.SimpleAuth.AuthenticatedRouteMixin

--- packages/ember-simple-auth/lib/mixins/authenticated_route_mixin.js
+++ packages/ember-simple-auth/lib/mixins/authenticated_route_mixin.js
@@ -1,4 +1,22 @@
+/**
+  The mixin for routes that require a user to be authenticated. When users hit a route that
+  implements this mixin and have not authenticated before, they will be redirected to the route
+  defined as `loginRoute` in Ember.SimpleAuth.setup (or `login` by default).
+
+  @class AuthenticatedRouteMixin
+  @namespace Ember.SimpleAuth
+  @extends Ember.Mixin
+  @static
+*/
 Ember.SimpleAuth.AuthenticatedRouteMixin = Ember.Mixin.create({
+  /**
+    This method implements the check for an authenticated user. In the case that no user is authenticated,
+    it redirects to the route defined as `loginRoute` in Ember.SimpleAuth.setup (or `login` by default).
+    It also intercepts the current transition so that it can be retried after the user
+    has authenticated (see Ember.SimpleAuth.ApplicationRouteMixin.loginSucceeded).
+
+    @method beforeModel
+  */
   beforeModel: function(transition) {
     if (!this.get('session.isAuthenticated')) {
       transition.abort();
@@ -6,6 +24,9 @@ Ember.SimpleAuth.AuthenticatedRouteMixin = Ember.Mixin.create({
     }
   },
 
+  /**
+    @private
+  */
   triggerLogin: function(transition) {
     this.set('session.attemptedTransition', transition);
     this.send('login');
