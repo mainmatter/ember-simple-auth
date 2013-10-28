e85d34f616e15fc1f81d745a73f8183fd7c396ff
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-10-28 19:38:26 +0100

    docs for Ember.SimpleAuth.LoginControllerMixin

--- packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
+++ packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
@@ -1,9 +1,40 @@
+/**
+  The mixin for the login controller. This controller sends the user's credentials to the
+  server and with the response sets up the current session (see Ember.SimpleAuth.Session.setup).
+
+  @class LoginControllerMixin
+  @namespace Ember.SimpleAuth
+  @extends Ember.Mixin
+  @static
+*/
 Ember.SimpleAuth.LoginControllerMixin = Ember.Mixin.create({
+  /**
+    This method takes the user's credentials and builds the request options as they are passed
+    Ember.$.ajax (see http://api.jquery.com/jQuery.ajax/). The default implementation follows
+    RFC 6749. In case you're using a custom server API you would want to override this method:
+
+    ```js
+      App.LoginController  = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin, {
+        tokenRequestOptions: function(username, password) {
+          var putData = '{ "SESSION": { "USER_NAME": "' + username + '", "PASS": "' + password + '" } }';
+          return { type: 'PUT', data: putData, contentType: 'application/json' };
+        }
+      });
+
+      @method tokenRequestOptions
+      @param {String} The user's username (or Email address or whatever is used to identify the user)
+      @param {String} The user's password
+      @return {Object} The request options to be passed to Ember.$.ajax (see http://api.jquery.com/jQuery.ajax/ for detailed documentation)
+    ```
+  */
   tokenRequestOptions: function(username, password) {
     var postData = ['grant_type=password', 'username=' + username, 'password=' + password].join('&');
     return { type: 'POST', data: postData, contentType: 'application/x-www-form-urlencoded' };
   },
   actions: {
+    /**
+      @private
+    */
     login: function() {
       var self = this;
       var data = this.getProperties('identification', 'password');
