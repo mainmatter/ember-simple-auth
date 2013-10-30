599e58801e0e012415ddf7483d4fa07d15208e4b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-10-30 20:28:51 +0100

    added method signatures

--- docs/theme/assets/js/main.js
+++ docs/theme/assets/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('body').scrollspy({ target: '#sidebar' });
+  $('body').scrollspy({ target: '.nav-sidebar' });
   $('#sidebar').on('activate.bs.scrollspy', function(event) {
     console.log(event);
   });
