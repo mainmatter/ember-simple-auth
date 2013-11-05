8d12aca60955fb3b27b081f70f4437708f999125
Gauthier Delacroix <gauthier.delacroix@gmail.com>
2013-11-05 19:28:46 +0100

    reset loggingIn after failed login

--- packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
+++ packages/ember-simple-auth/lib/mixins/login_controller_mixin.js
@@ -62,9 +62,11 @@ Ember.SimpleAuth.LoginControllerMixin = Ember.Mixin.create({
         self.set('loggingIn', true);
         var requestOptions = this.tokenRequestOptions(data.identification, data.password);
         Ember.$.ajax(Ember.SimpleAuth.serverTokenEndpoint, requestOptions).then(function(response) {
+          self.set('loggingIn', false);
           self.get('session').setup(response);
           self.send('loginSucceeded');
         }, function(xhr, status, error) {
+          self.set('loggingIn', false);
           self.send('loginFailed', xhr, status, error);
         });
       }
