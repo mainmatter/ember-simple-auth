0e555c33a415f3a785515c6e177b595c59f6bef5
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-11-26 22:51:08 +0100

    store was moved to session

--- packages/ember-simple-auth/tests/authenticators/base_test.js
+++ packages/ember-simple-auth/tests/authenticators/base_test.js
@@ -3,11 +3,3 @@ var authenticator;
 var StoreMock = Ember.Object.extend();
 
 module('Ember.SimpleAuth.Authenticators.Base');
-
-test('assigns a store on initialization', function() {
-  Ember.SimpleAuth.store = StoreMock;
-  authenticator          = Ember.SimpleAuth.Authenticators.Base.create();
-
-  console.log();
-  ok(authenticator.get('store') instanceof StoreMock, 'Ember.SimpleAuth.Authorizers.Base assigns an instance of the store class defined in Ember.SimpleAuth.store on creation');
-});
