9b5b7048e2eaf9949439c12a1cca9b8b21d08601
Marco Otte-Witte <mottewitte@rim.com>
2013-12-04 18:05:45 +0100

    ephemeral store actually stores stuff in-memory

--- packages/ember-simple-auth/lib/stores/ephemeral.js
+++ packages/ember-simple-auth/lib/stores/ephemeral.js
@@ -1,14 +1,24 @@
 'use strict';
 
 Ember.SimpleAuth.Stores.Ephemeral = Ember.Object.extend(Ember.Evented, {
+  init: function() {
+    this._data = {};
+  },
+
   restore: function() {
-    return {};
+    return jQuery.extend({}, this._data);
   },
 
   load: function(property) {
-    return undefined
+    return this._data[property];
   },
 
   save: function(properties) {
+    for (var property in properties) {
+      var value = properties[property];
+      if (Ember.isEmpty(value)) {
+        this._data[property] = value;
+      }
+    }
   }
 });
