eefadae960be579eabbdfdc52f4f44b4ab6eed6a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2013-12-12 19:13:10 +0100

    declare the attributes

--- packages/ember-simple-auth/lib/authorizers/oauth2.js
+++ packages/ember-simple-auth/lib/authorizers/oauth2.js
@@ -1,6 +1,8 @@
 'use strict';
 
 Ember.SimpleAuth.Authorizers.OAuth2 = Ember.Object.extend({
+  session: undefined,
+
   authorize: function(jqXHR, requestOptions) {
     if (!Ember.isEmpty(this.get('session.authToken'))) {
       jqXHR.setRequestHeader('Authorization', 'Bearer ' + this.get('session.authToken'));
