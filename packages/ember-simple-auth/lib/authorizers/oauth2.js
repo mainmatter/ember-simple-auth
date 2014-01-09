791e7a54e76d119cd52da7db5f61f997b7d74a7b
Marco Otte-Witte <mottewitte@rim.com>
2014-01-09 17:39:34 +0100

    API docs for OAuth2 authorizer

--- packages/ember-simple-auth/lib/authorizers/oauth2.js
+++ packages/ember-simple-auth/lib/authorizers/oauth2.js
@@ -1,6 +1,27 @@
 'use strict';
 
+/**
+  Authorizer that conforms to OAuth 2 (RFC 6749) by adding bearer tokens to all
+  requests (RFC 6750).
+
+  @class OAuth2
+  @namespace Ember.SimpleAuth.Authorizers
+  @extends Ember.SimpleAuth.Authorizers.Base
+  @constructor
+*/
 Ember.SimpleAuth.Authorizers.OAuth2 = Ember.SimpleAuth.Authorizers.Base.extend({
+  /**
+    Authorizes an XHR request by adding the `authToken` property from the
+    session as a bearer token in the `Authorization` header:
+
+    ```
+    Authorization: Bearer <token>
+    ```
+
+    @method authorize
+    @param {jqXHR} jqXHR The XHR request to authorize (see http://api.jquery.com/jQuery.ajax/#jqXHR)
+    @param {Object} requestOptions The options as provided to the `$.ajax` method (see http://api.jquery.com/jQuery.ajaxPrefilter/)
+  */
   authorize: function(jqXHR, requestOptions) {
     if (!Ember.isEmpty(this.get('session.authToken'))) {
       jqXHR.setRequestHeader('Authorization', 'Bearer ' + this.get('session.authToken'));
