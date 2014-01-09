253c8b3bb978357f5fe68239843e1c0e238789bf
Marco Otte-Witte <mottewitte@rim.com>
2014-01-09 18:14:57 +0100

    documentation for base store

--- packages/ember-simple-auth/lib/stores/base.js
+++ packages/ember-simple-auth/lib/stores/base.js
@@ -1,13 +1,52 @@
 'use strict';
 
+/**
+  The base for all store types. This serves as a starting point for
+  implementing custom stores and must not be used directly.
+
+  Stores may trigger the 'ember-simple-auth:session-updated' event when
+  any of the stored properties change (and the change wasn't triggered by the
+  `persist` method). The session listens to that event and will handle the
+  change accordingly.
+
+  @class Base
+  @namespace Ember.SimpleAuth.Stores
+  @extends Ember.Object
+  @uses Ember.Evented
+  @constructor
+*/
 Ember.SimpleAuth.Stores.Base = Ember.Object.extend(Ember.Evented, {
+  /**
+    Persists the `properties` in the store.
+
+    `Ember.SimpleAuth.Stores.Base`'s implementation does nothing.
+
+    @method persist
+    @param {Object} properties The properties to persist
+  */
   persist: function(properties) {
   },
 
+  /**
+    Restores all properties currently saved in the store.
+
+    `Ember.SimpleAuth.Stores.Base`'s implementation always returns an empty
+    plain Object.
+
+    @method restore
+    @return {Object} All properties currently persisted in the store.
+  */
   restore: function() {
     return {};
   },
 
+  /**
+    Clears the store.
+
+  `Ember.SimpleAuth.Stores.Base`'s implementation does nothing.
+
+    @method clear
+  */
   clear: function() {
   }
 });
