b9a56e6d9f0ef15c5a04b254d5f126cb73847e70
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-01-10 19:36:56 +0100

    API docs for the local storage store

--- packages/ember-simple-auth/lib/stores/local_storage.js
+++ packages/ember-simple-auth/lib/stores/local_storage.js
@@ -1,12 +1,38 @@
 'use strict';
 
+/**
+  Store that saves its data in the browser's `localStorage`.
+
+  @class LocalStorage
+  @namespace Ember.SimpleAuth.Stores
+  @extends Ember.SimpleAuth.Stores.Base
+  @constructor
+*/
 Ember.SimpleAuth.Stores.LocalStorage = Ember.SimpleAuth.Stores.Base.extend({
+  /**
+    The prefix to use for the store's keys so they can be distinguished from
+    other keys.
+
+    @property storageKeyPrefix
+    @type String
+    @default 'ember_simple_auth:'
+  */
   storageKeyPrefix: 'ember_simple_auth:',
 
+  /**
+    @method init
+    @private
+  */
   init: function() {
     this.bindToStorageEvents();
   },
 
+  /**
+    Persists the `properties` in the `localStorage`.
+
+    @method persist
+    @param {Object} properties The properties to persist
+  */
   persist: function(properties) {
     for (var property in properties) {
       var key = this.buildStorageKey(property);
@@ -14,6 +40,13 @@ Ember.SimpleAuth.Stores.LocalStorage = Ember.SimpleAuth.Stores.Base.extend({
     }
   },
 
+  /**
+    Restores all properties currently saved in the `localStorage` identified by
+    the `storageKeyPrefix`.
+
+    @method restore
+    @return {Object} All properties currently persisted in the session cookies
+  */
   restore: function() {
     var _this = this;
     var properties = {};
@@ -24,6 +57,12 @@ Ember.SimpleAuth.Stores.LocalStorage = Ember.SimpleAuth.Stores.Base.extend({
     return properties;
   },
 
+  /**
+    Clears the store by deleting all `localStorage` keys prefixed with the
+    `storageKeyPrefix`.
+
+    @method clear
+  */
   clear: function() {
     this.knownKeys().forEach(function(key) {
       localStorage.removeItem(key);
@@ -53,6 +92,10 @@ Ember.SimpleAuth.Stores.LocalStorage = Ember.SimpleAuth.Stores.Base.extend({
     return keys;
   },
 
+  /**
+    @method bindToStorageEvents
+    @private
+  */
   bindToStorageEvents: function() {
     var _this = this;
     Ember.$(window).bind('storage', function() {
