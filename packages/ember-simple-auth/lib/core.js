c221e6c5d63a2ac60215d09d459dd23930f9671a
Marco Otte-Witte <mottewitte@rim.com>
2014-02-21 10:49:53 +0100

    Revert "fixed API docs"
    This reverts commit 900e3ec6d859fe553f8e6bc356edb5df3f03ff46.

--- packages/ember-simple-auth/lib/core.js
+++ packages/ember-simple-auth/lib/core.js
@@ -78,7 +78,7 @@ Ember.SimpleAuth = Ember.Namespace.create({
       @param {String} [options.authenticationRoute] route to transition to for authentication - defaults to `'login'`
       @param {String} [options.routeAfterAuthentication] route to transition to after successful authentication - defaults to `'index'`
       @param {String} [options.routeAfterInvalidation] route to transition to after session invalidation - defaults to `'index'`
-      @param {Array[String]} [options.crossOriginWhitelist] Ember.SimpleAuth will never authorize requests going to a different origin than the one the Ember.js application was loaded from; to explicitely enable authorization for additional origins, whitelist those origins - defaults to `[]` _(beware that origins consist of protocol, host and port (port also has to be specified when the default ports 80 or 443 are used))_
+      @param {Array[String]} [options.crossOriginWhitelist] Ember.SimpleAuth will never authorize requests going to a different origin than the one the Ember.js application was loaded from; to explicitely enable authorization for additional origins, whitelist those origins - defaults to `[]` _(beware that origins consist of protocol, host and port (port can be left out when it is 80))_
       @param {Object} [options.authorizer] The authorizer _class_ to use; must extend `Ember.SimpleAuth.Authorizers.Base` - defaults to `Ember.SimpleAuth.Authorizers.OAuth2`
       @param {Object} [options.store] The store _class_ to use; must extend `Ember.SimpleAuth.Stores.Base` - defaults to `Ember.SimpleAuth.Stores.LocalStorage`
   **/
