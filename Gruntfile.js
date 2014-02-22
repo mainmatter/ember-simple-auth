7adc016bef0d5d3d985b5e833da775b11f857770
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-02-22 14:03:01 +0100

    fixed Gruntfile.jsm package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
   var emberSimpleAuthConfig = require('grunt-microlib').init.bind(this)(grunt);
   grunt.loadNpmTasks('grunt-microlib');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
 
   this.registerTask(
     'test:phantom',
@@ -18,9 +17,9 @@ module.exports = function(grunt) {
       namespace: 'EmberSimpleAuth'
     },
     env: process.env,
-    pkg: grunt.file.readJSON('package.json')/*,
+    pkg: grunt.file.readJSON('package.json'),
     browserify: require('./options/browserify.js'),
-    mocha_phantomjs: require('./options/mocha_phantom.js')*/
+    mocha_phantomjs: require('./options/mocha_phantom.js')
   };
 
   grunt.initConfig(grunt.util._.merge(emberSimpleAuthConfig, config));
