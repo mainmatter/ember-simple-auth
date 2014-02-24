34448169dad730aeee573d694c07c18f76f06739
Marco Otte-Witte <mottewitte@rim.com>
2014-02-24 11:41:20 +0100

    made grunt dist work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,147 @@
 module.exports = function(grunt) {
-  var emberSimpleAuthConfig = require('grunt-microlib').init.bind(this)(grunt);
-  grunt.loadNpmTasks('grunt-microlib');
+  this.registerTask('default', ['test']);
 
-  this.registerTask(
-    'test:phantom',
-    'Runs tests through the command line using PhantomJS',
-    ['build', 'tests']
-  );
+  this.registerTask('dist', 'Builds a distributable version of EmberSimpleAuth', [
+    'jshint',
+    'build',
+    'concat:amd',
+    'copy:sources',
+    'uglify:library',
+    'uglify:browser'
+  ]);
 
-  this.registerTask('test', ['build', 'tests']);
+  this.registerTask('build', 'Builds EmberSimpleAuth', [
+    'clean',
+    'transpile:amd'
+  ]);
 
-  var config = {
-    cfg: {
-      name: 'ember-simple-auth.js',
-      barename: 'ember-simple-auth',
-      namespace: 'EmberSimpleAuth'
-    },
-    env: process.env,
+  this.registerTask('build_tests', "Builds EmberSimpleAuth's tests", [
+    'build',
+    'transpile:tests'
+  ]);
+
+  this.registerTask('server', [
+    'tests',
+    'connect',
+    'watch'
+  ]);
+
+  this.registerTask('test', [
+    'jshint',
+    'tests',
+    'qunit'
+  ]);
+
+  grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    browserify: require('./options/browserify.js'),
-    mocha_phantomjs: require('./options/mocha_phantom.js')
-  };
 
-  grunt.initConfig(grunt.util._.merge(emberSimpleAuthConfig, config));
+    connect: {
+      server: {},
 
-  grunt.loadNpmTasks('grunt-browserify');
+      options: {
+        hostname: '0.0.0.0',
+        port: grunt.option('port') || 8000,
+        base: '.',
+        middleware: function(connect, options) {
+          return [
+            require('connect-redirection')(),
+            function(req, res, next) {
+              if (req.url === '/') {
+                res.redirect('/test');
+              } else {
+                next();
+              }
+            },
+            connect.static(options.base)
+          ];
+        }
+      }
+    },
+
+    watch: {
+      files: ['lib/**', 'test/**/*'],
+      tasks: ['build_tests', 'jshint']
+    },
+
+    transpile: {
+      amd: {
+        type: 'amd',
+        files: [{
+          expand: true,
+          cwd: 'lib/',
+          src: ['**/*.js'],
+          dest: 'tmp/'
+        }]
+      },
+
+      tests: {
+        type: 'amd',
+        files: [{
+          expand: true,
+          cwd: 'test/',
+          src: ['test_helpers.js', 'tests.js', 'tests/**/*_test.js'],
+          dest: 'tmp/'
+        }]
+      }
+    },
+
+    concat: {
+      amd: {
+        src: ['tmp/ember-simple-auth.js', 'tmp/ember-simple-auth/**/*.js'],
+        dest: 'tmp/ember-simple-auth.amd.js'
+      }
+    },
+
+    copy: {
+      sources: {
+        files: [{
+          expand: true,
+          cwd: 'tmp/',
+          src: ['<%= pkg.name %>.js', '<%= pkg.name %>.amd.js', '<%= pkg.name %>.amd.js.map', '<%= pkg.name %>/**/*.js', 'vendor/**/*.js'],
+          dest: 'dist/sources-<%= pkg.version %>/'
+        }, {
+          expand: true,
+          src: ['wrap/*', 'vendor/**/*.js'],
+          dest: 'dist/sources-<%= pkg.version %>/'
+        }]
+      }
+    },
+
+    uglify: {
+      library: {
+        files: {
+          'dist/<%= pkg.name %>-<%= pkg.version %>.amd.min.js': ['dist/sources-<%= pkg.version %>/<%= pkg.name %>.amd.js']
+        }
+      },
+      browser: {
+        files: {
+          'dist/<%= pkg.name %>-<%= pkg.version %>.min.js': ['dist/sources-<%= pkg.version %>/<%= pkg.name %>.amd.js']
+        }
+      }
+    },
+
+    jshint: {
+      library: 'lib/**/*.js',
+      tests: 'test/tests/**/*.js',
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    clean: ['dist', 'tmp'],
+
+    qunit: {
+      all: ['test/index.html']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-es6-module-transpiler');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 };
