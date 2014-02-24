948b0d7b6c1aecf3ba14f135a6b5aed2826747a4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-02-24 23:14:44 +0100

    make the examples real HTML files

--- examples/middleware.js
+++ examples/middleware.js
@@ -1,7 +1,3 @@
 module.exports = function(request, response, next) {
-  if (request.url === '/ember-simple-auth.js') {
-    response.redirect('/dist/ember-simple-auth.js');
-  } else {
-    next();
-  }
+  next();
 };
