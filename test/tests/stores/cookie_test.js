160543c2a0875764db8c941d72370b21d6d3f359
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-02-27 22:23:02 +0100

    converted store specs

--- test/tests/stores/cookie_test.js
+++ test/tests/stores/cookie_test.js
@@ -8,73 +8,34 @@ describe('Stores.Cookie', function() {
   });
 
   storeBehavior();
-});
 
-/*var store;
+  describe('the "ember-simple-auth:session-updated" event', function() {
+    beforeEach(function() {
+      var _this = this;
+      _this.triggered = false;
+      this.store.persist({ key: 'value' });
+      _this.store.one('ember-simple-auth:session-updated', function() {
+        _this.triggered = true;
+      });
+    });
 
-module('Stores.Cookie', {
-  setup: function() {
-    store = Cookie.create();
-  },
-  teardown: function() {
-    store.clear();
-  }
-});
+    it('is not triggered when the store changes the cookie itself', function() {
+      this.store.syncProperties();
 
-test('clears itself', function() {
-  store.persist({ key1: 'value1', key2: 'value2' });
-  store.clear();
+      expect(this.triggered).to.be(false);
+    });
 
-  equal(store.restore().key1, null, 'Ember.SimpleAuth.Stores.Cookie deletes all properties when it is cleared.');
-  equal(store.restore().key2, null, 'Ember.SimpleAuth.Stores.Cookie deletes all properties when it is cleared.');
-});
+    it('is not triggered when nothing actually changes in the cookie', function() {
+      this.store.syncProperties();
 
-test('loads all properties', function() {
-  deepEqual(store.restore(), {}, 'Ember.SimpleAuth.Stores.Cookie returns an empty plain object when all properties are loaded but the store is empty');
+      expect(this.triggered).to.be(false);
+    });
 
-  store.persist({ key1: 'value1', key2: 'value2' });
-  deepEqual(store.restore(), { key1: 'value1', key2: 'value2' }, 'Ember.SimpleAuth.Stores.Cookie loads all stored properties.');
-});
+    it('is triggered when the cookie is changed outside of the store', function() {
+      document.cookie = 'ember_simple_auth:key=other value;';
+      this.store.syncProperties();
 
-test('saves properties', function() {
-  store.persist({ key: 'value' });
-  equal(store.restore().key, 'value', 'Ember.SimpleAuth.Stores.Cookie saves a property and loads it again.');
-
-  store.persist({ key1: 'value1', key2: 'value2' });
-  equal(store.restore().key1, 'value1', 'Ember.SimpleAuth.Stores.Cookie saves multiple properties.');
-  equal(store.restore().key2, 'value2', 'Ember.SimpleAuth.Stores.Cookie saves multiple properties.');
-  equal(store.restore().key, 'value', 'Ember.SimpleAuth.Stores.Cookie does not destroy previously stored properties when it saves others.');
-});
-
-test('does not trigger an event for changes through its API', function() {
-  var triggered;
-  store.persist({ key3: 'value' });
-  store.one('ember-simple-auth:session-updated', function() {
-    triggered = true;
+      expect(this.triggered).to.be(true);
+    });
   });
-  store.syncProperties();
-
-  ok(!triggered, 'Ember.SimpleAuth.Stores.Cookie does not trigger the "ember-simple-auth:session-updated" event when the change was made through its API.');
 });
-
-test('recognizes when the cookies change', function() {
-  var triggered;
-  store.persist({ key: 'value' });
-  document.cookie = 'ember_simple_auth:key=other value;';
-  store.one('ember-simple-auth:session-updated', function() {
-    triggered = true;
-  });
-  store.syncProperties();
-
-  equal(store.restore().key, 'other value', 'Ember.SimpleAuth.Stores.Cookie recognizes when the cookies changes.');
-  ok(triggered, 'Ember.SimpleAuth.Stores.Cookie triggers the "ember-simple-auth:session-updated" event when the cookies changes.');
-
-  triggered = false;
-  store.one('ember-simple-auth:session-updated', function() {
-    triggered = true;
-  });
-  store.syncProperties();
-
-  ok(!triggered, 'Ember.SimpleAuth.Stores.Cookie does not trigger the "ember-simple-auth:session-updated" event when no cookies actually changed.');
-});
-*/
