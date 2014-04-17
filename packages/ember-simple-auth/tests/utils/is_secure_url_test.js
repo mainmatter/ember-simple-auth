8f330430856d96a65396863c498c30cfd334c5fd
Marco Otte-Witte <mottewitte@rim.com>
2014-04-17 13:01:12 +0200

    removed extra namespace wrap in test

--- packages/ember-simple-auth/tests/utils/is_secure_url_test.js
+++ packages/ember-simple-auth/tests/utils/is_secure_url_test.js
@@ -1,13 +1,11 @@
 import { isSecureUrl } from 'ember-simple-auth/utils/is_secure_url';
 
-describe('Util', function() {
-  describe('#isSecureUrl', function() {
-    it('is true for "https://example.com"', function() {
-      expect(isSecureUrl("https://example.com")).to.be.true;
-    });
+describe('#isSecureUrl', function() {
+  it('is true for "https://example.com"', function() {
+    expect(isSecureUrl("https://example.com")).to.be.true;
+  });
 
-    it('is false for "http://example.com"', function() {
-      expect(isSecureUrl("http://example.com")).to.be.false;
-    });
+  it('is false for "http://example.com"', function() {
+    expect(isSecureUrl("http://example.com")).to.be.false;
   });
 });
