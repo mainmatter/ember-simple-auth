30c1da758643dc61e664cb744e35d3833117e910
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-06-23 16:03:32 +0200

    handle session event correctly in ApplicationRouteMixin, closes #199

--- packages/ember-simple-auth/lib/ember-simple-auth/mixins/application_route_mixin.js
+++ packages/ember-simple-auth/lib/ember-simple-auth/mixins/application_route_mixin.js
@@ -57,7 +57,8 @@ export default Ember.Mixin.create({
     ]).forEach(function(event) {
       _this.get(Configuration.sessionPropertyName).on(event, function(error) {
         Array.prototype.unshift.call(arguments, event);
-        _this.send.apply(_this, arguments);
+        var target = transition.isActive ? transition : _this;
+        target.send.apply(target, arguments);
       });
     });
   },
