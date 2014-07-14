aef51255ff65b02eaf3d29fd35f248a72dcd4f35
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-07-14 14:51:33 +0200

    added test helpers

---
+++ packages/ember-simple-auth/lib/simple-auth/test-helpers/authenticate-session.js
@@ -0,0 +1,5 @@
+export default Ember.Test.registerAsyncHelper('authenticateSession', function(app) {
+  var session = app.__container__.lookup('simple-auth-session:main');
+  session.authenticate('simple-auth-authenticator:test');
+  return wait();
+});
