7ade2e48912503020aeba70bee0cd3681dcc3a77
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-07-23 14:04:24 +0200

    fixed dist build for ember-simple-auth-testing

--- packages/ember-simple-auth-testing/lib/simple-auth-testing/ember.js
+++ packages/ember-simple-auth-testing/lib/simple-auth-testing/ember.js
@@ -1,6 +1,3 @@
-var global = (typeof window !== 'undefined') ? window : {},
-    Ember = global.Ember;
-
 import initializer from './initializer';
 
 Ember.onLoad('Ember.Application', function(Application) {
