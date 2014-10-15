392d6da3d8ef3c4c605546d18b5e28057081d3a6
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-10-15 09:15:33 +0200

    cleaned up objectsAreEqual for readability

--- packages/ember-simple-auth/lib/simple-auth/utils/objects-are-equal.js
+++ packages/ember-simple-auth/lib/simple-auth/utils/objects-are-equal.js
@@ -2,22 +2,42 @@
   @method objectsAreEqual
   @private
 */
-export default function(a, b) {
-  if(a === b) return true;
-  if(!(a instanceof Object) || !(b instanceof Object)) return false;
-  if(a.constructor !== b.constructor) return false;
-
-  for(var p in a) {
-    if(!a.hasOwnProperty(p)) continue;
-    if(!b.hasOwnProperty(p)) return false;
-    if(a[p] === b[p]) continue;
-    if(typeof(a[p]) !== 'object') return false;
-    if (!arguments.callee(a[p], b[p])) return false;
+function objectsAreEqual(a, b) {
+  if (a === b) {
+    return true;
+  }
+  if (!(a instanceof Object) || !(b instanceof Object)) {
+    return false;
+  }
+  if(a.constructor !== b.constructor) {
+    return false;
   }
 
-  for(p in b) {
-    if(b.hasOwnProperty(p) && !a.hasOwnProperty(p)) return false;
+  for (var property in a) {
+    if (!a.hasOwnProperty(property)) {
+      continue;
+    }
+    if (!b.hasOwnProperty(property)) {
+      return false;
+    }
+    if (a[property] === b[property]) {
+      continue;
+    }
+    if (Ember.typeOf(a[property]) !== 'object') {
+      return false;
+    }
+    if (!objectsAreEqual(a[property], b[property])) {
+      return false;
+    }
+  }
+
+  for (property in b) {
+    if (b.hasOwnProperty(property) && !a.hasOwnProperty(property)) {
+      return false;
+    }
   }
 
   return true;
 }
+
+export default objectsAreEqual;
