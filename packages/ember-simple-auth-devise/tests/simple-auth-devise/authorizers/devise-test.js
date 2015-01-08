e146cb584227b27e56c4dd987a03a8149051905e
Sebastian Szturo <s.szturo@me.com>
2015-01-08 18:24:54 +0900

    change devise user_token to token

--- packages/ember-simple-auth-devise/tests/simple-auth-devise/authorizers/devise-test.js
+++ packages/ember-simple-auth-devise/tests/simple-auth-devise/authorizers/devise-test.js
@@ -45,16 +45,16 @@ describe('Devise', function() {
         this.authorizer.set('session.isAuthenticated', true);
       });
 
-      context('when the session contains a non empty user_token and user_email', function() {
+      context('when the session contains a non empty token and user_email', function() {
         beforeEach(function() {
-          this.authorizer.set('session.user_token', 'secret token!');
+          this.authorizer.set('session.token', 'secret token!');
           this.authorizer.set('session.user_email', 'user@email.com');
         });
 
-        it('adds the "user_token" and "user_email" query string fields to the request', function() {
+        it('adds the "token" and "user_email" query string fields to the request', function() {
           this.authorizer.authorize(this.request, {});
 
-          expect(this.request.setRequestHeader).to.have.been.calledWith('Authorization', 'Token user_token="secret token!", user_email="user@email.com"');
+          expect(this.request.setRequestHeader).to.have.been.calledWith('Authorization', 'Token token="secret token!", user_email="user@email.com"');
         });
       });
 
@@ -85,9 +85,9 @@ describe('Devise', function() {
         });
       });
 
-      context('when the session does not contain an user_token', function() {
+      context('when the session does not contain an token', function() {
         beforeEach(function() {
-          this.authorizer.set('session.user_token', null);
+          this.authorizer.set('session.token', null);
         });
 
         itDoesNotAuthorizeTheRequest();
