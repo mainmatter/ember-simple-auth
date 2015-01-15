2a261afa9cdfe6caf84399d5dad5054d9a7280db
Duke <srdukex@gmail.com>
2015-01-15 19:08:51 -0200

    docs for crossOriginWhitelist subdomain wildcard support

--- packages/ember-simple-auth/lib/simple-auth/configuration.js
+++ packages/ember-simple-auth/lib/simple-auth/configuration.js
@@ -133,6 +133,8 @@ export default {
     this setting. _Beware that origins consist of protocol, host and port (port
     can be left out when it is 80 for HTTP or 443 for HTTPS)_, e.g.
     `http://domain.com:1234`, `https://external.net`. You can also whitelist
+    all subdomains for a specific domain using wildcard e.g.
+    `http://*.domain.com:1234`, `https://*.external.net` or  whitelist
     all external origins by specifying `[*]`.
 
     @property crossOriginWhitelist
