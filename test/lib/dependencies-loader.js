a9c0a82dc427906656c4d7e31a3f709a228835de
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-01-21 21:17:53 +0100

    run tests against newest versions by default

--- test/lib/dependencies-loader.js
+++ test/lib/dependencies-loader.js
@@ -4,8 +4,8 @@
     return (window.location.search.match(regexp) || [])[1];
   }
 
-  var jQueryVersion = parseVersion('jQuery') || '2.1.1';
-  var emberVersion  = parseVersion('ember') || '1.8.0';
+  var jQueryVersion = parseVersion('jQuery') || '2.1.3';
+  var emberVersion  = parseVersion('ember') || '1.9.1';
 
   document.write('<script src="vendor/jquery-' + jQueryVersion + '.js"></script>');
   document.write('<script src="vendor/handlebars.js"></script>');
