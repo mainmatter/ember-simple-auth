90f0d3efd07050dae4f00eba478de197cc442ecb
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-02-05 20:38:53 +0100

    fixed cookie store's read method, close #417

--- packages/ember-simple-auth-cookie-store/lib/simple-auth-cookie-store/stores/cookie.js
+++ packages/ember-simple-auth-cookie-store/lib/simple-auth-cookie-store/stores/cookie.js
@@ -151,7 +151,7 @@ export default Base.extend({
     @private
   */
   read: function() {
-    var value = document.cookie.match(new RegExp(this.cookieName + name + '=([^;]+)')) || [];
+    var value = document.cookie.match(new RegExp(this.cookieName + '=([^;]+)')) || [];
     return decodeURIComponent(value[1] || '');
   },
 
