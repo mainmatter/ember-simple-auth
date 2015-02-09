ecc7581ee8f4ebc7240df2322c3195a8150353c4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-02-09 15:37:41 +0100

    test against Ember 1.10.0 by default

--- test/lib/dependencies-loader.js
+++ test/lib/dependencies-loader.js
@@ -5,7 +5,7 @@
   }
 
   var jQueryVersion     = parseVersion('jQuery') || '2.1.3';
-  var emberVersion      = parseVersion('ember') || '1.9.1';
+  var emberVersion      = parseVersion('ember') || '1.10.0';
   var handlebarsVersion = parseVersion('handlebars') || '2.0.0';
 
   document.write('<script src="vendor/jquery-' + jQueryVersion + '.js"></script>');
