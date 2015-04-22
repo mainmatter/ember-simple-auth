44d563592b0114a6d16dc22f1937def690d971b6
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-04-22 20:26:54 +0200

    better equality checking algorithm

--- packages/ember-simple-auth/lib/simple-auth/utils/objects-are-equal.js
+++ packages/ember-simple-auth/lib/simple-auth/utils/objects-are-equal.js
@@ -1,43 +1,55 @@
 /**
+  This was taken from http://stackoverflow.com/questions/1068834/object-comparison-in-javascript#1144249
+
   @method objectsAreEqual
   @private
 */
-function objectsAreEqual(a, b) {
-  if (a === b) {
+export default function objectsAreEqual(a, b) {
+  function compare(x, y) {
+    var property;
+    if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+      return true;
+    }
+
+    if (x === y) {
+      return true;
+    }
+
+    if (!(x instanceof Object && y instanceof Object)) {
+      return false;
+    }
+
+    for (property in y) {
+      if (y.hasOwnProperty(property) !== x.hasOwnProperty(property)) {
+        return false;
+      } else if (typeof y[property] !== typeof x[property]) {
+        return false;
+      }
+    }
+
+    for (property in x) {
+      if (y.hasOwnProperty(property) !== x.hasOwnProperty(property)) {
+        return false;
+      } else if (typeof y[property] !== typeof x[property]) {
+        return false;
+      }
+
+      switch (typeof (x[property])) {
+        case 'object':
+          if (!compare(x[property], y[property])) {
+            return false;
+          }
+          break;
+        default:
+          if (x[property] !== y[property]) {
+            return false;
+          }
+          break;
+      }
+    }
+
     return true;
   }
-  if (!(a instanceof Object) || !(b instanceof Object)) {
-    return false;
-  }
-  if(a.constructor !== b.constructor) {
-    return false;
-  }
 
-  for (var property in a) {
-    if (!a.hasOwnProperty(property)) {
-      continue;
-    }
-    if (!b.hasOwnProperty(property)) {
-      return false;
-    }
-    if (a[property] === b[property]) {
-      continue;
-    }
-    if (Ember.typeOf(a[property]) !== 'object') {
-      return false;
-    }
-    if (!objectsAreEqual(a[property], b[property])) {
-      return false;
-    }
-  }
-
-  for (property in b) {
-    if (b.hasOwnProperty(property) && !a.hasOwnProperty(property)) {
-      return false;
-    }
-  }
-
-  return true;
+  return compare(a, b);
 }
-
-export default objectsAreEqual;
