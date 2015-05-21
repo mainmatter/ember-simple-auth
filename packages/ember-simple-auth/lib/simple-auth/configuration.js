679615904a318d2765a48e0c4dcc74ef051c39cc
Eli Flanagan <efx@users.noreply.github.com>
2015-05-21 09:14:26 -0400

    typo fix

--- packages/ember-simple-auth/lib/simple-auth/configuration.js
+++ packages/ember-simple-auth/lib/simple-auth/configuration.js
@@ -82,7 +82,7 @@ export default {
     see
     [Ember's API docs](http://emberjs.com/api/classes/Ember.Application.html#method_register);
     when the application does not interact with a server that requires
-    authorized requests, no auzthorizer is needed.
+    authorized requests, no authorizer is needed.
 
     @property authorizer
     @readOnly
