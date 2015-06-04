eb094956de7f3be007e8b9941846c9af34836ec1
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-04 14:16:15 +0200

    fixed tests

--- addon/mixins/unauthenticated-route-mixin.js
+++ addon/mixins/unauthenticated-route-mixin.js
@@ -39,7 +39,7 @@ export default Ember.Mixin.create({
   beforeModel: function(transition) {
     if (this.get(Configuration.base.sessionPropertyName).get('isAuthenticated')) {
       transition.abort();
-      Ember.assert('The route configured as Configuration.routeIfAlreadyAuthenticated cannot implement the UnauthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.routeIfAlreadyAuthenticated);
+      Ember.assert('The route configured as Configuration.routeIfAlreadyAuthenticated cannot implement the UnauthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.base.routeIfAlreadyAuthenticated);
       this.transitionTo(Configuration.base.routeIfAlreadyAuthenticated);
     }
   }
