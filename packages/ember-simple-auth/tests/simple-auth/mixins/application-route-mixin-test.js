780786f3ea3de81b7599423ba2313c27cd11b509
Kevin Bullaughey <kbullaughey@gmail.com>
2015-06-05 10:51:57 -0400

    make separate test for _authRouteEntryComplete flag; remove unnecessary assertion

--- packages/ember-simple-auth/tests/simple-auth/mixins/application-route-mixin-test.js
+++ packages/ember-simple-auth/tests/simple-auth/mixins/application-route-mixin-test.js
@@ -44,13 +44,17 @@ describe('ApplicationRouteMixin', function() {
       });
 
       it("translates the session's 'sessionAuthenticationSucceeded' event into an action invocation", function(done) {
-        expect(this.route.get('_authRouteEntryComplete')).to.be.true;
         this.session.trigger('sessionAuthenticationSucceeded');
 
         Ember.run.next(this, function() {
           expect(this.route.send).to.have.been.calledWith('sessionAuthenticationSucceeded');
           done();
         });
+      });
+
+      it("sets _authRouteEntryComplete so we know actions can be sent to the route", function(done) {
+        expect(this.route.get('_authRouteEntryComplete')).to.be.true;
+        done();
       });
 
       it("sends the action to the transition on a new route instance", function(done) {
@@ -63,7 +67,6 @@ describe('ApplicationRouteMixin', function() {
         route2.beforeModel(transition);
         this.session.trigger('sessionAuthenticationSucceeded');
         Ember.run.next(this, function() {
-          expect(route2.get('_authRouteEntryComplete')).to.not.be.true;
           expect(transition.send).to.have.been.calledWith('sessionAuthenticationSucceeded');
           expect(route2.send).to.not.have.been.called;
           done();
