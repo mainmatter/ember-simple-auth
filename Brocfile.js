2f548adfa8043f6f52ebb4a30871a53f0ffe38b3
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-15 15:41:56 +0200

    added ember-suave

--- Brocfile.js
+++ Brocfile.js
@@ -3,19 +3,22 @@
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-var app = new EmberAddon();
+var app = new EmberAddon({
+  jscsOptions: {
+    enabled: true,
+    testGenerator: function(relativePath, errors) {
+      if (errors) {
+        errors = "\\n" + this.escapeErrorString(errors);
+      } else {
+        errors = "";
+      }
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+      return "describe('JSCS - " + relativePath + "', function(){\n" +
+        "it('should pass jscs', function() { \n" +
+        "  expect(" + !errors + ", '" + relativePath + " should pass jscs." + errors + "').to.be.ok; \n" +
+        "})});\n";
+    }
+  }
+});
 
 module.exports = app.toTree();
