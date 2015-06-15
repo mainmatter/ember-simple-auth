62a43e8b9c77201593c92a3f30611503020242e0
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-15 15:37:48 +0200

    test cleanup

--- tests/unit/stores/shared/store-behavior.js
+++ tests/unit/stores/shared/store-behavior.js
@@ -4,26 +4,31 @@ import Ember from 'ember';
 
 export default function(options) {
   var syncExternalChanges = (options || {}).syncExternalChanges || Ember.K;
+  var store;
 
   describe('#persist', function() {
-    it('persists an object', function() {
-      this.store.persist({ key: 'value' });
+    beforeEach(() => {
+       store = (options || {}).store();
+    });
 
-      expect(this.store.restore()).to.eql({ key: 'value' });
+    it('persists an object', function() {
+      store.persist({ key: 'value' });
+
+      expect(store.restore()).to.eql({ key: 'value' });
     });
 
     it('overrides existing data', function() {
-      this.store.persist({ key1: 'value1' });
-      this.store.persist({ key2: 'value2' });
+      store.persist({ key1: 'value1' });
+      store.persist({ key2: 'value2' });
 
-      expect(this.store.restore()).to.eql({ key2: 'value2' });
+      expect(store.restore()).to.eql({ key2: 'value2' });
     });
 
     it('does not trigger the "sessionDataUpdated" event', function(done) {
       var triggered = false;
-      this.store.one('sessionDataUpdated', function() { triggered = true; });
-      this.store.persist({ key: 'other value' });
-      syncExternalChanges.apply(this);
+      store.one('sessionDataUpdated', function() { triggered = true; });
+      store.persist({ key: 'other value' });
+      syncExternalChanges.call();
 
       Ember.run.next(function() {
         expect(triggered).to.be.false;
@@ -35,34 +40,34 @@ export default function(options) {
   describe('#restore', function() {
     context('when the store is empty', function() {
       it('returns an empty object', function() {
-        expect(this.store.restore()).to.eql({});
+        expect(store.restore()).to.eql({});
       });
     });
 
     context('when the store has data', function() {
       beforeEach(function() {
-        this.store.persist({ key1: 'value1', key2: 'value2' });
+        store.persist({ key1: 'value1', key2: 'value2' });
       });
 
       it('returns all data in the store', function() {
-        expect(this.store.restore()).to.eql({ key1: 'value1', key2: 'value2' });
+        expect(store.restore()).to.eql({ key1: 'value1', key2: 'value2' });
       });
 
       it('returns a copy of the stored data', function() {
-        var data = this.store.restore();
+        var data = store.restore();
         data.key1 = 'another value!';
 
-        expect(this.store.restore()).to.eql({ key1: 'value1', key2: 'value2' });
+        expect(store.restore()).to.eql({ key1: 'value1', key2: 'value2' });
       });
     });
   });
 
   describe('#clear', function() {
     it('empties the store', function() {
-      this.store.persist({ key1: 'value1', key2: 'value2' });
-      this.store.clear();
+      store.persist({ key1: 'value1', key2: 'value2' });
+      store.clear();
 
-      expect(this.store.restore()).to.eql({});
+      expect(store.restore()).to.eql({});
     });
   });
 }
