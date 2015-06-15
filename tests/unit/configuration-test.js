3ce7eca8ccf4f48c65620b95b6991881489ea1b4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-15 14:35:12 +0200

    test cleanup

--- tests/unit/configuration-test.js
+++ tests/unit/configuration-test.js
@@ -2,259 +2,255 @@
 import { it } from 'ember-mocha';
 import Configuration from 'ember-simple-auth/configuration';
 
-describe('Configuration', function() {
-  beforeEach(function() {
-    this.container     = { lookup: function() {} };
-    this.router        = { get: function() { return 'rootURL'; } };
-    this.containerStub = sinon.stub(this.container, 'lookup');
-    this.containerStub.withArgs('router:main').returns(this.router);
+let container;
+let router;
+
+describe('Configuration', () => {
+  beforeEach(() => {
+    container         = { lookup: function() {} };
+    router            = { get: function() { return 'rootURL'; } };
+    let containerStub = sinon.stub(container, 'lookup');
+    containerStub.withArgs('router:main').returns(router);
   });
 
-  describe('authenticationRoute', function() {
-    it('defaults to "login"', function() {
+  afterEach(() => {
+    Configuration.load(container, {});
+  });
+
+  describe('authenticationRoute', () => {
+    it('defaults to "login"', () => {
       expect(Configuration.base.authenticationRoute).to.eql('login');
     });
   });
 
-  describe('routeAfterAuthentication', function() {
-    it('defaults to "index"', function() {
+  describe('routeAfterAuthentication', () => {
+    it('defaults to "index"', () => {
       expect(Configuration.base.routeAfterAuthentication).to.eql('index');
     });
   });
 
-  describe('routeIfAlreadyAuthenticated', function() {
-    it('defaults to "index"', function() {
+  describe('routeIfAlreadyAuthenticated', () => {
+    it('defaults to "index"', () => {
       expect(Configuration.base.routeIfAlreadyAuthenticated).to.eql('index');
     });
   });
 
-  describe('sessionPropertyName', function() {
-    it('defaults to "session"', function() {
+  describe('sessionPropertyName', () => {
+    it('defaults to "session"', () => {
       expect(Configuration.base.sessionPropertyName).to.eql('session');
     });
   });
 
-  describe('authorizer', function() {
-    it('defaults to null', function() {
+  describe('authorizer', () => {
+    it('defaults to null', () => {
       expect(Configuration.base.authorizer).to.be.null;
     });
   });
 
-  describe('session', function() {
+  describe('session', () => {
     it('defaults to "simple-auth-session:main"', function() {
       expect(Configuration.base.session).to.eq('simple-auth-session:main');
     });
   });
 
-  describe('store', function() {
+  describe('store', () => {
     it('defaults to "simple-auth-session-store:local-storage"', function() {
       expect(Configuration.base.store).to.eq('simple-auth-session-store:local-storage');
     });
   });
 
-  describe('localStorageKey', function() {
-    it('defaults to "ember_simple_auth:session"', function() {
+  describe('localStorageKey', () => {
+    it('defaults to "ember_simple_auth:session"', () => {
       expect(Configuration.base.localStorageKey).to.eql('ember_simple_auth:session');
     });
   });
 
-  describe('crossOriginWhitelist', function() {
-    it('defaults to []', function() {
+  describe('crossOriginWhitelist', () => {
+    it('defaults to []', () => {
       expect(Configuration.base.crossOriginWhitelist).to.be.a('array');
       expect(Configuration.base.crossOriginWhitelist).to.be.empty;
     });
   });
 
-  describe('cookie.domain', function() {
-    it('defaults to null', function() {
+  describe('cookie.domain', () => {
+    it('defaults to null', () => {
       expect(Configuration.cookie.domain).to.be.null;
     });
   });
 
-  describe('cookie.name', function() {
-    it('defaults to "ember_simple_auth:session"', function() {
+  describe('cookie.name', () => {
+    it('defaults to "ember_simple_auth:session"', () => {
       expect(Configuration.cookie.name).to.eql('ember_simple_auth:session');
     });
   });
 
-  describe('cookie.expirationTime', function() {
-    it('defaults to null', function() {
+  describe('cookie.expirationTime', () => {
+    it('defaults to null', () => {
       expect(Configuration.cookie.expirationTime).to.be.null;
     });
   });
 
-  describe('serverTokenEndpoint', function() {
-    it('defaults to "/users/sign_in"', function() {
+  describe('serverTokenEndpoint', () => {
+    it('defaults to "/users/sign_in"', () => {
       expect(Configuration.devise.serverTokenEndpoint).to.eql('/users/sign_in');
     });
   });
 
-  describe('resourceName', function() {
-    it('defaults to "user"', function() {
+  describe('resourceName', () => {
+    it('defaults to "user"', () => {
       expect(Configuration.devise.resourceName).to.eq('user');
     });
   });
 
-  describe('tokenAttributeName', function() {
-    it('defaults to "token"', function() {
+  describe('tokenAttributeName', () => {
+    it('defaults to "token"', () => {
       expect(Configuration.devise.tokenAttributeName).to.eql('token');
     });
   });
 
-  describe('identificationAttributeName', function() {
-    it('defaults to "email"', function() {
+  describe('identificationAttributeName', () => {
+    it('defaults to "email"', () => {
       expect(Configuration.devise.identificationAttributeName).to.eq('email');
     });
   });
 
-  describe('serverTokenEndpoint', function() {
-    it('defaults to "/token"', function() {
+  describe('serverTokenEndpoint', () => {
+    it('defaults to "/token"', () => {
       expect(Configuration.oauth2.serverTokenEndpoint).to.eql('/token');
     });
   });
 
-  describe('serverTokenRevocationEndpoint', function() {
-    it('defaults to null', function() {
+  describe('serverTokenRevocationEndpoint', () => {
+    it('defaults to null', () => {
       expect(Configuration.oauth2.serverTokenRevocationEndpoint).to.be.null;
     });
   });
 
-  describe('refreshAccessTokens', function() {
-    it('defaults to true', function() {
+  describe('refreshAccessTokens', () => {
+    it('defaults to true', () => {
       expect(Configuration.oauth2.refreshAccessTokens).to.be.true;
     });
   });
 
-  describe('.load', function() {
-    beforeEach(function() {
-      this.container   = { lookup: function() {} };
-      this.router      = { get: function() { return 'rootURL'; } };
-      this.containerStub = sinon.stub(this.container, 'lookup');
-      this.containerStub.withArgs('router:main').returns(this.router);
-    });
-
-    it("sets applicationRootUrl to the application's root URL", function() {
-      Configuration.load(this.container, {});
+  describe('.load', () => {
+    it("sets applicationRootUrl to the application's root URL", () => {
+      Configuration.load(container, {});
 
       expect(Configuration.base.applicationRootUrl).to.eql('rootURL');
     });
 
-    it('sets authenticationRoute correctly', function() {
-      Configuration.load(this.container, { base: { authenticationRoute: 'authenticationRoute' } });
+    it('sets authenticationRoute correctly', () => {
+      Configuration.load(container, { base: { authenticationRoute: 'authenticationRoute' } });
 
       expect(Configuration.base.authenticationRoute).to.eql('authenticationRoute');
     });
 
-    it('sets routeAfterAuthentication correctly', function() {
-      Configuration.load(this.container, { base: { routeAfterAuthentication: 'routeAfterAuthentication' } });
+    it('sets routeAfterAuthentication correctly', () => {
+      Configuration.load(container, { base: { routeAfterAuthentication: 'routeAfterAuthentication' } });
 
       expect(Configuration.base.routeAfterAuthentication).to.eql('routeAfterAuthentication');
     });
 
-    it('sets routeIfAlreadyAuthenticated correctly', function() {
-      Configuration.load(this.container, { base: { routeIfAlreadyAuthenticated: 'routeIfAlreadyAuthenticated' } });
+    it('sets routeIfAlreadyAuthenticated correctly', () => {
+      Configuration.load(container, { base: { routeIfAlreadyAuthenticated: 'routeIfAlreadyAuthenticated' } });
 
       expect(Configuration.base.routeIfAlreadyAuthenticated).to.eql('routeIfAlreadyAuthenticated');
     });
 
-    it('sets sessionPropertyName correctly', function() {
-      Configuration.load(this.container, { base: { sessionPropertyName: 'sessionPropertyName' } });
+    it('sets sessionPropertyName correctly', () => {
+      Configuration.load(container, { base: { sessionPropertyName: 'sessionPropertyName' } });
 
       expect(Configuration.base.sessionPropertyName).to.eql('sessionPropertyName');
     });
 
-    it('sets authorizer correctly', function() {
-      Configuration.load(this.container, { base: { authorizer: 'authorizer' } });
+    it('sets authorizer correctly', () => {
+      Configuration.load(container, { base: { authorizer: 'authorizer' } });
 
       expect(Configuration.base.authorizer).to.eql('authorizer');
     });
 
-    it('sets session correctly', function() {
-      Configuration.load(this.container, { base: { session: 'session' } });
+    it('sets session correctly', () => {
+      Configuration.load(container, { base: { session: 'session' } });
 
       expect(Configuration.base.session).to.eql('session');
     });
 
-    it('sets store correctly', function() {
-      Configuration.load(this.container, { base: { store: 'store' } });
+    it('sets store correctly', () => {
+      Configuration.load(container, { base: { store: 'store' } });
 
       expect(Configuration.base.store).to.eql('store');
     });
 
-    it('sets localStorageKey correctly', function() {
-      Configuration.load(this.container, { base: { localStorageKey: 'localStorageKey' } });
+    it('sets localStorageKey correctly', () => {
+      Configuration.load(container, { base: { localStorageKey: 'localStorageKey' } });
 
       expect(Configuration.base.localStorageKey).to.eql('localStorageKey');
     });
 
-    it('sets crossOriginWhitelist correctly', function() {
-      Configuration.load(this.container, { base: { crossOriginWhitelist: ['https://some.origin:1234'] } });
+    it('sets crossOriginWhitelist correctly', () => {
+      Configuration.load(container, { base: { crossOriginWhitelist: ['https://some.origin:1234'] } });
 
       expect(Configuration.base.crossOriginWhitelist).to.eql(['https://some.origin:1234']);
     });
 
-    it('sets cookieDomain correctly', function() {
-      Configuration.load(this.container, { cookie: { domain: '.example.com' } });
+    it('sets cookieDomain correctly', () => {
+      Configuration.load(container, { cookie: { domain: '.example.com' } });
 
       expect(Configuration.cookie.domain).to.eql('.example.com');
     });
 
-    it('sets cookieName correctly', function() {
-      Configuration.load(this.container, { cookie: { name: 'cookieName' } });
+    it('sets cookieName correctly', () => {
+      Configuration.load(container, { cookie: { name: 'cookieName' } });
 
       expect(Configuration.cookie.name).to.eql('cookieName');
     });
 
-    it('sets cookieExpirationTime correctly', function() {
-      Configuration.load(this.container, { cookie: { expirationTime: 1 } });
+    it('sets cookieExpirationTime correctly', () => {
+      Configuration.load(container, { cookie: { expirationTime: 1 } });
 
       expect(Configuration.cookie.expirationTime).to.eql(1);
     });
 
-    it('sets serverTokenEndpoint correctly', function() {
-      Configuration.load(this.container, { devise: { serverTokenEndpoint: 'serverTokenEndpoint' } });
+    it('sets serverTokenEndpoint correctly', () => {
+      Configuration.load(container, { devise: { serverTokenEndpoint: 'serverTokenEndpoint' } });
 
       expect(Configuration.devise.serverTokenEndpoint).to.eql('serverTokenEndpoint');
     });
 
-    it('sets resourceName correctly', function() {
-      Configuration.load(this.container, { devise: { resourceName: 'resourceName' } });
+    it('sets resourceName correctly', () => {
+      Configuration.load(container, { devise: { resourceName: 'resourceName' } });
 
       expect(Configuration.devise.resourceName).to.eql('resourceName');
     });
 
-    it('sets identificationAttributeName correctly', function() {
-      Configuration.load(this.container, { devise: { identificationAttributeName: 'identificationAttributeName' } });
+    it('sets identificationAttributeName correctly', () => {
+      Configuration.load(container, { devise: { identificationAttributeName: 'identificationAttributeName' } });
 
       expect(Configuration.devise.identificationAttributeName).to.eql('identificationAttributeName');
     });
 
-    it('sets tokenAttributeName correctly', function() {
-      Configuration.load(this.container, { devise: { tokenAttributeName: 'tokenAttributeName' } });
+    it('sets tokenAttributeName correctly', () => {
+      Configuration.load(container, { devise: { tokenAttributeName: 'tokenAttributeName' } });
 
       expect(Configuration.devise.tokenAttributeName).to.eql('tokenAttributeName');
     });
 
-    it('sets serverTokenEndpoint correctly', function() {
-      Configuration.load(this.container, { oauth2: { serverTokenEndpoint: 'serverTokenEndpoint' } });
+    it('sets serverTokenEndpoint correctly', () => {
+      Configuration.load(container, { oauth2: { serverTokenEndpoint: 'serverTokenEndpoint' } });
 
       expect(Configuration.oauth2.serverTokenEndpoint).to.eql('serverTokenEndpoint');
     });
 
-    it('sets serverTokenRevocationEndpoint correctly', function() {
-      Configuration.load(this.container, { oauth2: { serverTokenRevocationEndpoint: 'serverTokenRevocationEndpoint' } });
+    it('sets serverTokenRevocationEndpoint correctly', () => {
+      Configuration.load(container, { oauth2: { serverTokenRevocationEndpoint: 'serverTokenRevocationEndpoint' } });
 
       expect(Configuration.oauth2.serverTokenRevocationEndpoint).to.eql('serverTokenRevocationEndpoint');
     });
 
-    it('sets refreshAccessTokens correctly', function() {
-      Configuration.load(this.container, { oauth2: { refreshAccessTokens: false } });
+    it('sets refreshAccessTokens correctly', () => {
+      Configuration.load(container, { oauth2: { refreshAccessTokens: false } });
 
       expect(Configuration.oauth2.refreshAccessTokens).to.be.false;
     });
-  });
-
-  afterEach(function() {
-    Configuration.load(this.container, {});
   });
 });
