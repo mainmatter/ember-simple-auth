646ec340a06272aa742fd132e6b3ef544dab9134
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-17 18:20:56 +0200

    fixed example app

--- tests/dummy/app/controllers/login.js
+++ tests/dummy/app/controllers/login.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   actions: {
     authenticate() {
       let data = this.getProperties('identification', 'password');
-      this.get('session').authenticate('simple-auth-authenticator:oauth2-password-grant', data).catch((error) => {
+      this.get('session').authenticate('authenticator:oauth2-password-grant', data).catch((error) => {
         this.set('error', error.error);
       });
     }
