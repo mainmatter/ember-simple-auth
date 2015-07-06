3a7134674b6a0f62b6e3aa70c2734bbb097c187a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-07-06 21:09:18 +0200

    added session service

--- tests/dummy/app/controllers/login.js
+++ tests/dummy/app/controllers/login.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sessionService: Ember.inject.service('session'),
+
   actions: {
     authenticate() {
       let data = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2-password-grant', data).catch((reason) => {
+      this.get('sessionService').authenticate('authenticator:oauth2-password-grant', data).catch((reason) => {
         this.set('errorMessage', reason.error);
       });
     }
