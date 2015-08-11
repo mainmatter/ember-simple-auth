da8209905592b39cb1631c8d0e87220f8c9e393c
Michael de Hoog <michael.dehoog@gmail.com>
2015-08-11 16:43:31 +1000

    Removed asynchronous call of this._super (fixes #608)

--- addon/instance-initializers/setup-session-restoration.js
+++ addon/instance-initializers/setup-session-restoration.js
@@ -1,13 +1,14 @@
-import Ember from 'ember';
-
 export default function setupSessionRestoration(instance) {
   const { container } = instance;
   const applicationRoute = container.lookup('route:application');
   const session = container.lookup('simple-auth-session:main');
   applicationRoute.reopen({
-    beforeModel(transition) {
-      const superCall = Ember.run.bind(this, this._super, transition);
-      return session.restore().then(superCall, superCall);
+    beforeModel() {
+      const superResult = this._super(...arguments);
+      const superReturn = function() {
+        return superResult;
+      };
+      return session.restore().then(superReturn, superReturn);
     }
   });
 }
