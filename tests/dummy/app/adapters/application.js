f6c9358ab67ba86563ee2a27dd6f9add57baf94a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-17 20:48:07 +0200

    added DataAdapterMixin
    This is meant to be used in Ember Data adapters and will make
    authorizing requests as easy as with the $.ajaxPrefilter before while
    being much cleaner. All that is to be done to authorize all Ember Data
    requests with an authorizer of choice is now:
    
    import DS from 'ember-data';
    import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
    
    export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
      authorizer: 'authorizer:application'
    });
    
    while the authorizer is defined as:
    
    import OAuth2Bearer from 'ember-simple-auth/authorizers/oauth2-bearer';
    
    export default OAuth2Bearer.extend();

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,31 +1,6 @@
-import Ember from 'ember';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-const { service } = Ember.inject;
-
-export default DS.JSONAPIAdapter.extend({
-  session: service('session'),
-
-  ajaxOptions() {
-    let hash = this._super(...arguments);
-    let { beforeSend } = hash;
-    hash.beforeSend = (xhr) => {
-      this.get('session').authorize('authorizer:application', (headerName, headerValue) => {
-        xhr.setRequestHeader(headerName, headerValue);
-      });
-      if (beforeSend) {
-        beforeSend(xhr);
-      }
-    };
-    return hash;
-  },
-
-  handleResponse(status) {
-    if (status === 401) {
-      this.get('session').invalidate();
-      return true;
-    } else {
-      return this._super(...arguments);
-    }
-  }
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:application'
 });
