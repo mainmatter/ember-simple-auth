5820545adf43878a1464d8ba72b45b777bc897f5
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-19 20:47:27 +0200

    call the original beforeModel, not _super

--- addon/instance-initializers/setup-session-restoration.js
+++ addon/instance-initializers/setup-session-restoration.js
@@ -2,10 +2,10 @@ export default function setupSessionRestoration(instance) {
   const { container } = instance;
   const applicationRoute = container.lookup('route:application');
   const session = container.lookup('session:main');
+  const originalMethod = applicationRoute.beforeModel.bind(originalMethod);
   applicationRoute.reopen({
-    beforeModel(transition) {
-      const boundSuper = this._super.bind(this);
-      return session.restore().then(() => boundSuper(transition), () => boundSuper(transition));
+    beforeModel() {
+      return session.restore().then(() => originalMethod(...arguments), () => originalMethod(...arguments));
     }
   });
 }
