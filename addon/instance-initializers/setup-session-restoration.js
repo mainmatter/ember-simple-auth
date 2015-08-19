aaed6b09ce7d7939d1ce6aaa441175fbb7c976ba
Michael de Hoog <michael.dehoog@gmail.com>
2015-08-19 17:47:02 +1000

    Attempt session restoration before running application route's `beforeModel` hook (improved #608 fix)

--- addon/instance-initializers/setup-session-restoration.js
+++ addon/instance-initializers/setup-session-restoration.js
@@ -4,8 +4,8 @@ export default function setupSessionRestoration(instance) {
   const session = container.lookup('session:main');
   applicationRoute.reopen({
     beforeModel() {
-      const superResult = this._super(...arguments);
-      return session.restore().then(() => superResult, () => superResult);
+      const boundSuper = this._super.bind(this);
+      return session.restore().then(() => boundSuper(...arguments), () => boundSuper(...arguments));
     }
   });
 }
