a7248dc977a0ed65ee67a9a6476f6152925654ad
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-08-19 21:17:23 +0200

    fixed binding and specs

--- addon/instance-initializers/setup-session-restoration.js
+++ addon/instance-initializers/setup-session-restoration.js
@@ -2,7 +2,7 @@ export default function setupSessionRestoration(instance) {
   const { container } = instance;
   const applicationRoute = container.lookup('route:application');
   const session = container.lookup('session:main');
-  const originalMethod = applicationRoute.beforeModel.bind(originalMethod);
+  const originalMethod = applicationRoute.beforeModel.bind(applicationRoute);
   applicationRoute.reopen({
     beforeModel() {
       return session.restore().then(() => originalMethod(...arguments), () => originalMethod(...arguments));
