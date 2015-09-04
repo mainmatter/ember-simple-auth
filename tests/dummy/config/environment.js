c101e9281c04736a65cbe5f98a9f0c0a456e89f3
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-04 21:55:58 +0200

    fixed csp warning in dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'style-src': "'self' 'unsafe-inline'",
     }
   };
 
