35ff350095921705541ac6b4b05e5a69d704dff4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-07 22:17:32 +0200

    Revert "handle the case when the app doesn't define an application route"
    This reverts commit b73d38b6a521a4f15a61b0dfccfd407370bca833.

--- addon/instance-initializers/setup-session-restoration.js
+++ addon/instance-initializers/setup-session-restoration.js
@@ -1,21 +1,8 @@
-import Ember from 'ember';
-
-const APPLICATION_ROUTE_KEY = 'route:application';
-
-function ensureApplicationRoute(container) {
-  let applicationRoute = container.lookup(APPLICATION_ROUTE_KEY);
-  if (!applicationRoute) {
-    applicationRoute = Ember.Route.extend();
-    container.register(APPLICATION_ROUTE_KEY, applicationRoute);
-  }
-  return applicationRoute;
-}
-
 export default function setupSessionRestoration(instance) {
   const { container } = instance;
-  const applicationRoute = ensureApplicationRoute(container);
+  const applicationRoute = container.lookup('route:application');
   const session = container.lookup('session:main');
-  const originalBeforeModel = applicationRoute.beforeModel || Ember.K;
+  const originalBeforeModel = applicationRoute.beforeModel;
   const applyOriginalBeforeModel = function() {
     return originalBeforeModel.apply(applicationRoute, arguments);
   };
