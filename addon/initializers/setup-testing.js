a33ab94835023123ed7dc8b213d51b938b4dd91f
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-07 17:30:24 +0200

    register the test authenticator lazily

--- addon/initializers/setup-testing.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import Test from '../authenticators/test';
-
-export default function setupTesting(registry) {
-  if (Ember.testing) {
-    registry.register('authenticator:test', Test);
-  }
-}
