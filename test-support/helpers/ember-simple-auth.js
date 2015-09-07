7cce0846115396acfc12f5c0cf3eb42e3a4295b6
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-07 17:59:28 +0200

    Revert "register the test authenticator lazily"
    This reverts commit 89c41d32b14c978412725d0490590e36bdac5159.

--- test-support/helpers/ember-simple-auth.js
+++ test-support/helpers/ember-simple-auth.js
@@ -1,19 +1,6 @@
-import Test from 'ember-simple-auth/authenticators/test';
-
-const TEST_CONTAINER_KEY = 'authenticator:test';
-
-function ensureAuthenticator(container) {
-  const authenticator = container.lookup(TEST_CONTAINER_KEY);
-  if (!authenticator) {
-    container.register(TEST_CONTAINER_KEY, Test);
-  }
-}
-
 export function authenticateSession(app, sessionData) {
-  const { __container__: container } = app;
-  const session = container.lookup('service:session');
-  ensureAuthenticator(container);
-  session.authenticate(TEST_CONTAINER_KEY, sessionData);
+  const session = app.__container__.lookup('service:session');
+  session.authenticate('authenticator:test', sessionData);
   return wait();
 };
 
