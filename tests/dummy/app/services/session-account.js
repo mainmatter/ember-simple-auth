1a3582a169ce125b96915db63f9a568b3a981a90
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-14 20:55:33 +0200

    cleanup of dummy app

--- tests/dummy/app/services/session-account.js
+++ tests/dummy/app/services/session-account.js
@@ -4,7 +4,7 @@ const { service } = Ember.inject;
 
 export default Ember.Service.extend({
   session: service('session'),
-  store: Ember.inject.service(),
+  store: service(),
 
   account: Ember.computed('session.data.authenticated.account_id', function() {
     const accountId = this.get('session.data.authenticated.account_id');
