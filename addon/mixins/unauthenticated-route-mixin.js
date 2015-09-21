edd54a3f4968ca400c0320f8cfdb0bf1e67d36dd
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-21 21:12:52 +0200

    added docs for mixins

--- addon/mixins/unauthenticated-route-mixin.js
+++ addon/mixins/unauthenticated-route-mixin.js
@@ -4,12 +4,17 @@ import Configuration from './../configuration';
 const { service } = Ember.inject;
 
 /**
-  This mixin is for routes that should only be accessible if the session is
-  not authenticated. This is e.g. the case for the login route that should not
-  be accessible when the session is already authenticated. Including this mixin
+  This mixin is for routes that are only accessible if the session is not
+  authenticated. This is e.g. the case for the login route that is not
+  accessible when the session is already authenticated. Including this mixin
   in a route automatically adds a hook that redirects to the
-  [`Configuration.routeIfAlreadyAuthenticated`](#SimpleAuth-Configuration-routeIfAlreadyAuthenticated),
-  which defaults to `'index'`.
+  {{#crossLink "Configuration/routeIfAlreadyAuthenticated:property"}}{{/crossLink}}
+  if the session is already authenticated.
+
+  The `UnauthenticatedRouteMixin` performs the check for whether the session is
+  already authenticated in the `beforeModel` method. __If `beforeModel` is
+  overridden in a route that also uses this mixin, ensure that the custom
+  implementation calls `this._super(...arguments)`__.
 
   ```js
   // app/routes/login.js
@@ -18,25 +23,28 @@ const { service } = Ember.inject;
   export default Ember.Route.extend(UnauthenticatedRouteMixin);
   ```
 
-  `UnauthenticatedRouteMixin` performs the redirect in the `beforeModel`
-  method. __If `beforeModel` is overridden, ensure that the custom
-  implementation calls `this._super(transition)`__.
-
   @class UnauthenticatedRouteMixin
-  @namespace Mixins
   @module ember-simple-auth/mixins/unauthenticated-route-mixin
   @extends Ember.Mixin
   @static
   @public
 */
 export default Ember.Mixin.create({
+  /**
+    The session service.
+
+    @property session
+    @type SessionService
+    @public
+  */
   session: service('session'),
 
   /**
     This method implements the enforcement of the session not being
     authenticated. If the session is authenticated, the current transition will
-    be aborted and a redirect will be triggered to the
-    [`Configuration.routeIfAlreadyAuthenticated`](#SimpleAuth-Configuration-routeIfAlreadyAuthenticated).
+    be aborted and a redirect to the
+    {{#crossLink "Configuration/routeIfAlreadyAuthenticated:property"}}{{/crossLink}}
+    will be triggered.
 
     @method beforeModel
     @param {Transition} transition The transition that lead to this route
