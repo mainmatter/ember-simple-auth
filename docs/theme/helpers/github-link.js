f6c1e9fd584a027a7882fbe6e8bf7bba76f50b51
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-09-22 21:05:29 +0200

    use a partial for the source links

--- docs/theme/helpers/github-link.js
+++
@@ -1,7 +0,0 @@
-/*global self:true */
-module.exports = {
-  githubLink: function(context, githubUrl, projectVersion, file, line) {
-    console.log(context.githubUrl);
-    return githubUrl + '/blob/' + projectVersion + '/' + file + '#L' + line;
-  }
-};
