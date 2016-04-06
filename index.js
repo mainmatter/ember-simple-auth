d0e8f322566e9278e8a2c233d1cc70ea1f682f12
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-06 14:50:35 +0200

    Allow for nesting ember-simple-auth in another addon

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ module.exports = {
   name: 'ember-simple-auth',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
 
     app.import('vendor/ember-simple-auth/register-version.js');
   }
