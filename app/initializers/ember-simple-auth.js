ba3735d7897edc9cd5d6ff0c069228a1ecf0bace
Josh Tumath <josh.tumath@outlook.com>
2016-04-11 23:42:01 +0100

    Updated to suggested changes

--- app/initializers/ember-simple-auth.js
+++ app/initializers/ember-simple-auth.js
@@ -7,12 +7,8 @@ import setupSessionService from 'ember-simple-auth/initializers/setup-session-se
 export default {
   name:       'ember-simple-auth',
   initialize: function(registry) {
-    const config = ENV['ember-simple-auth'] || {};
-    
-    // We need to use the default Ember baseURL if one hasn't been set
-    if (!(ENV['ember-simple-auth'] && ENV['ember-simple-auth'].baseURL)) {
-      config.baseURL = ENV.baseURL;
-    }
+    const config   = ENV['ember-simple-auth'] || {};
+    config.baseURL = config.baseURL || ENV.baseURL;
     
     Configuration.load(config);
 
