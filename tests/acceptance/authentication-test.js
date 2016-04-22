b199b9c86657b9d3e246926d7ec999139cec9c48
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-04-22 16:21:15 +0200

    fix tests

--- tests/acceptance/authentication-test.js
+++ tests/acceptance/authentication-test.js
@@ -6,6 +6,7 @@ import startApp from '../helpers/start-app';
 import Pretender from 'pretender';
 import { invalidateSession, authenticateSession, currentSession } from '../helpers/ember-simple-auth';
 import destroyApp from '../helpers/destroy-app';
+import config from '../../config/environment';
 
 describe('Acceptance: Authentication', function() {
   let application;
@@ -32,7 +33,7 @@ describe('Acceptance: Authentication', function() {
 
     it('can be visited when the session is authenticated', () => {
       server = new Pretender(function() {
-        this.get('/posts', () => [200, { 'Content-Type': 'application/json' }, '{"data":[]}']);
+        this.get(`${config.apiEndpoint}/posts`, () => [200, { 'Content-Type': 'application/json' }, '{"data":[]}']);
       });
       authenticateSession(application, { userId: 1, otherData: 'some-data' });
 
