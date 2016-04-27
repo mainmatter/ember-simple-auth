5448c3e92986027964ad6bbbb0d1cddcb4daf0bf
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-04-27 20:44:10 +0200

    [fastboot-compatibility] fix route mixin transitions

--- addon/session-stores/cookie.js
+++ addon/session-stores/cookie.js
@@ -148,6 +148,7 @@ export default BaseStore.extend({
     if (isEmpty(data)) {
       return RSVP.resolve({});
     } else {
+      data = decodeURIComponent(data);
       return RSVP.resolve(JSON.parse(data));
     }
   },
