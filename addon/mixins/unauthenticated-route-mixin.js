9124f5528a120f6a0a04a6d01be8b9bf0d869323
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-04-28 11:27:05 +0200

    don't abort transitions in FastBoot

--- addon/mixins/unauthenticated-route-mixin.js
+++ addon/mixins/unauthenticated-route-mixin.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import getOwner from 'ember-getowner-polyfill';
 import Configuration from './../configuration';
 
-const { service } = Ember.inject;
+const { inject: { service }, computed } = Ember;
 
 /**
   __This mixin is used to make routes accessible only if the session is
@@ -34,6 +35,12 @@ export default Ember.Mixin.create({
   */
   session: service('session'),
 
+  _isFastBoot: computed(function() {
+    const fastboot = getOwner(this).lookup('service:fastboot');
+
+    return fastboot ? fastboot.get('isFastBoot') : false;
+  }),
+
   /**
     Checks whether the session is authenticated and if it is aborts the current
     transition and instead transitions to the
@@ -49,7 +56,11 @@ export default Ember.Mixin.create({
   */
   beforeModel(transition) {
     if (this.get('session').get('isAuthenticated')) {
-      transition.abort();
+
+      if (!this.get('_isFastBoot')) {
+        transition.abort();
+      }
+
       Ember.assert('The route configured as Configuration.routeIfAlreadyAuthenticated cannot implement the UnauthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.routeIfAlreadyAuthenticated);
       this.transitionTo(Configuration.routeIfAlreadyAuthenticated);
     } else {
