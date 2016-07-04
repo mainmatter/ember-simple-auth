7a0a3b226fe79425f3ead055ca529f72518cffce
Justin Bull <me@justinbull.ca>
2016-07-04 14:46:04 -0400

    Add rejectWithXhr to optionally reject with XHR vs response body (#1012)
    Allows ember apps using ember-simple-auth to receive the whole XHR object if the backend fails, instead of the response body, if they so choose.
    
    In the case of OAuth 2.0 backends, it's been a pattern in the wild to use X- headers to send context as to why a grant has failed. Examples include API throttling, brute force lockouts, and OTP/two-factor authentication information.
    
    Selfishly, I require this change so my application can be notified when the API has locked out an account due to suspicious activity via an X- header.
    
    The decision to expose it as an option was chosen so backwards compatibility is maintained and keeps the addon simple for those who need not be concerned with complex backends.

--- tests/unit/authenticators/oauth2-password-grant-test.js
+++ tests/unit/authenticators/oauth2-password-grant-test.js
@@ -190,13 +190,31 @@ describe('OAuth2PasswordGrantAuthenticator', () => {
 
     describe('when the authentication request fails', () => {
       beforeEach(() => {
-        server.post('/token', () => [400, { 'Content-Type': 'application/json' }, '{ "error": "invalid_grant" }']);
+        server.post('/token', () => [400, { 'Content-Type': 'application/json', 'X-Custom-Context': 'foobar' }, '{ "error": "invalid_grant" }']);
       });
 
       it('rejects with the correct error', (done) => {
         authenticator.authenticate('username', 'password').catch((error) => {
           expect(error).to.eql({ error: 'invalid_grant' });
           done();
+        });
+      });
+
+      describe('when reject with XHR is enabled', () => {
+        beforeEach(() => {
+          authenticator.set('rejectWithXhr', true);
+        });
+
+        it('rejects with xhr object', () => {
+          return authenticator.authenticate('username', 'password').catch((error) => {
+            expect(error.responseJSON).to.eql({ error: 'invalid_grant' });
+          });
+        });
+
+        it('provides access to custom headers', () => {
+          return authenticator.authenticate('username', 'password').catch((error) => {
+            expect(error.getResponseHeader('X-Custom-Context')).to.eql('foobar');
+          });
         });
       });
     });
