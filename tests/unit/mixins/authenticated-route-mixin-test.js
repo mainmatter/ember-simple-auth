54476e1c312fd7cd866242ae8dd289cbbe06bbdd
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-07-28 23:26:28 +0200

    fix my messed up rebase 😳

--- tests/unit/mixins/authenticated-route-mixin-test.js
+++ tests/unit/mixins/authenticated-route-mixin-test.js
@@ -9,11 +9,7 @@ import InternalSession from 'ember-simple-auth/internal-session';
 import Configuration from 'ember-simple-auth/configuration';
 import EphemeralStore from 'ember-simple-auth/session-stores/ephemeral';
 
-<<<<<<< HEAD
-const { Mixin, RSVP, Route: EmberRoute } = Ember;
-=======
 const { Mixin, RSVP, Route } = Ember;
->>>>>>> 1acbecf... Fastboot Feature Branch (#1032)
 
 describe('AuthenticatedRouteMixin', () => {
   let route;
@@ -27,15 +23,6 @@ describe('AuthenticatedRouteMixin', () => {
           return RSVP.resolve('upstreamReturnValue');
         }
       });
-<<<<<<< HEAD
-      const Route = EmberRoute.extend(MixinImplementingBeforeModel, AuthenticatedRouteMixin, {
-        // pretend this is never FastBoot
-        _isFastBoot: false,
-        // replace actual transitionTo as the router isn't set up etc.
-        transitionTo() {}
-      });
-=======
->>>>>>> 1acbecf... Fastboot Feature Branch (#1032)
 
       session = InternalSession.create({ store: EphemeralStore.create() });
       transition = {
@@ -44,11 +31,8 @@ describe('AuthenticatedRouteMixin', () => {
       };
 
       route = Route.extend(MixinImplementingBeforeModel, AuthenticatedRouteMixin, {
-<<<<<<< HEAD
-=======
         // pretend this is never FastBoot
         _isFastBoot: false,
->>>>>>> 1acbecf... Fastboot Feature Branch (#1032)
         // replace actual transitionTo as the router isn't set up etc.
         transitionTo() {}
       }).create({ session });
