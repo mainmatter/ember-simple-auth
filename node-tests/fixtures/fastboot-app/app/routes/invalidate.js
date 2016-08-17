2bd16a7fa6ccf5b1b118708ad993856e85e4e091
Josemar Luedke <josemarluedke@gmail.com>
2016-08-17 05:53:51 -0700

    [Fastboot] Integration testing for fastboot (#1045)
    * Add session register back
    
    * Create a fastboot test app and create basic tests against fastboot.

---
+++ node-tests/fixtures/fastboot-app/app/routes/invalidate.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+const { Route, inject: { service } } = Ember;
+
+export default Route.extend(AuthenticatedRouteMixin, {
+  session: service(),
+
+  model() {
+    this.get('session').invalidate();
+  }
+});
