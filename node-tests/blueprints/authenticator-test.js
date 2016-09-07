0fa628c78a81597d154ff9d78bcb78413ba42eac
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-09-07 20:12:44 +0200

    fix node tests

--- node-tests/blueprints/authenticator-test.js
+++ node-tests/blueprints/authenticator-test.js
@@ -1,61 +1,53 @@
 'use strict';
 
 var EOL                = require('os').EOL;
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
-var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
-var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks     = BlueprintHelpers.setupTestHooks;
+var emberNew           = BlueprintHelpers.emberNew;
+var generateAndDestroy = BlueprintHelpers.emberGenerateDestroy;
+
+var expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
 describe('Acceptance: ember generate and destroy authenticator', function() {
   setupTestHooks(this);
-  
+
   it('generates a torii authenticator', function() {
-    return generateAndDestroy(['authenticator', 'application', '--base-class=torii'], {
-      files: [
-        { file: 'app/authenticators/application.js', contains: ['\
-import Ember from \'ember\';' + EOL + '\
+    return emberNew().then(() => generateAndDestroy(['authenticator', 'application', '--base-class=torii'], (file) => {
+      expect(file('app/authenticators/application.js')).to.contain('\
 import Torii from \'ember-simple-auth/authenticators/torii\';' + EOL + '\
 ' + EOL + '\
 export default Torii.extend({' + EOL + '\
   torii: Ember.inject.service(\'torii\')' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('generates an OAuth 2.0 authenticator', function() {
-    return generateAndDestroy(['authenticator', 'application', '--base-class=oauth2'], {
-      files: [
-        { file: 'app/authenticators/application.js', contains: ['\
-import Ember from \'ember\';' + EOL + '\
+    return emberNew().then(() => generateAndDestroy(['authenticator', 'application', '--base-class=oauth2'], (file) => {
+      expect(file('app/authenticators/application.js')).to.contain('\
 import OAuth2PasswordGrant from \'ember-simple-auth/authenticators/oauth2-password-grant\';' + EOL + '\
 ' + EOL + '\
 export default OAuth2PasswordGrant.extend({' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('generates a devise authenticator', function() {
-    return generateAndDestroy(['authenticator', 'application', '--base-class=devise'], {
-      files: [
-        { file: 'app/authenticators/application.js', contains: ['\
-import Ember from \'ember\';' + EOL + '\
+    return emberNew().then(() => generateAndDestroy(['authenticator', 'application', '--base-class=devise'], (file) => {
+      expect(file('app/authenticators/application.js')).to.contain('\
 import Devise from \'ember-simple-auth/authenticators/devise\';' + EOL + '\
 ' + EOL + '\
 export default Devise.extend({' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('generates a generic authenticator', function() {
-    return generateAndDestroy(['authenticator', 'application'], {
-      files: [
-        { file: 'app/authenticators/application.js', contains: ['\
-import Ember from \'ember\';' + EOL + '\
+    return emberNew().then(() => generateAndDestroy(['authenticator', 'application'], (file) => {
+      expect(file('app/authenticators/application.js')).to.contain('\
 import Base from \'ember-simple-auth/authenticators/base\';' + EOL + '\
 ' + EOL + '\
 export default Base.extend({' + EOL + '\
@@ -68,26 +60,23 @@ export default Base.extend({' + EOL + '\
   invalidate(data) {' + EOL + '\
   }' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('throws when the authenticator is specified as its own base class', function() {
-    return generateAndDestroy(['authenticator', 'application', '--base-class=application'], {
-      throws: { 
-        message: 'Authenticators cannot extend from themself. Remove the --base-class option or specify one of "oauth2", "torii" or "devise".',
-        type: 'SilentError'
-      }
-    });
+    return expect(emberNew().then(() => {
+      return generateAndDestroy(['authenticator', 'application', '--base-class=application'])
+    })).to.eventually.be.rejectedWith(
+      'Authenticators cannot extend from themself. Remove the --base-class option or specify one of "oauth2", "torii" or "devise".'
+    );
   });
 
   it('throws when an unknown base class is specified', function() {
-    return generateAndDestroy(['authenticator', 'application', '--base-class=unknown'], {
-      throws: { 
-        message: 'The authenticator base class "unknown" is unknown. Remove the --base-class option or specify one of "oauth2", "torii" or "devise".',
-        type: 'SilentError'
-      }
-    });
+    return expect(emberNew().then(() => {
+      return generateAndDestroy(['authenticator', 'application', '--base-class=unknown'])
+    })).to.eventually.be.rejectedWith(
+      'The authenticator base class "unknown" is unknown. Remove the --base-class option or specify one of "oauth2", "torii" or "devise".'
+    );
   });
 });
--- node-tests/blueprints/authorizer-test.js
+++ node-tests/blueprints/authorizer-test.js
@@ -1,69 +1,64 @@
 'use strict';
 
 var EOL                = require('os').EOL;
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
-var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
-var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks     = BlueprintHelpers.setupTestHooks;
+var emberNew           = BlueprintHelpers.emberNew;
+var generateAndDestroy = BlueprintHelpers.emberGenerateDestroy;
+
+var expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
 describe('Acceptance: ember generate and destroy authorizer', function() {
   setupTestHooks(this);
   
   it('generates an OAuth 2.0 authorizer', function() {
-    return generateAndDestroy(['authorizer', 'application', '--base-class=oauth2'], {
-      files: [
-        { file: 'app/authorizers/application.js', contains: ['\
+    return emberNew().then(() => generateAndDestroy(['authorizer', 'application', '--base-class=oauth2'], (file) => {
+      expect(file('app/authorizers/application.js')).to.contain('\
 import OAuth2Bearer from \'ember-simple-auth/authorizers/oauth2-bearer\';' + EOL + '\
 ' + EOL + '\
 export default OAuth2Bearer.extend({' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('generates a devise authorizer', function() {
-    return generateAndDestroy(['authorizer', 'application', '--base-class=devise'], {
-      files: [
-        { file: 'app/authorizers/application.js', contains: ['\
+    return emberNew().then(() => generateAndDestroy(['authorizer', 'application', '--base-class=devise'], (file) => {
+      expect(file('app/authorizers/application.js')).to.contain('\
 import Devise from \'ember-simple-auth/authorizers/devise\';' + EOL + '\
 ' + EOL + '\
 export default Devise.extend({' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('generates a generic authorizer', function() {
-    return generateAndDestroy(['authorizer', 'application'], {
-      files: [
-        { file: 'app/authorizers/application.js', contains: ['\
+    return emberNew().then(() => generateAndDestroy(['authorizer', 'application'], (file) => {
+      expect(file('app/authorizers/application.js')).to.contain('\
 import Base from \'ember-simple-auth/authorizers/base\';' + EOL + '\
 ' + EOL + '\
 export default Base.extend({' + EOL + '\
   authorize(/*data, block*/) {' + EOL + '\
   }' + EOL + '\
 });' + EOL + '\
-']}
-      ]
-    });
+');
+    }));
   });
 
   it('throws when the authorizer is specified as its own base class', function() {
-    return generateAndDestroy(['authorizer', 'application', '--base-class=application'], {
-      throws: { 
-        message: 'Authorizers cannot extend from themself. Remove the --base-class option or specify one of "oauth2" or "devise".',
-        type: 'SilentError'
-      }
-    });
+    return expect(emberNew().then(() => {
+      return generateAndDestroy(['authorizer', 'application', '--base-class=application'])
+    })).to.eventually.be.rejectedWith(
+      'Authorizers cannot extend from themself. Remove the --base-class option or specify one of "oauth2" or "devise".'
+    );
   });
 
   it('throws when an unknown base class is specified', function() {
-    return generateAndDestroy(['authorizer', 'application', '--base-class=unknown'], {
-      throws: { 
-        message: 'The authorizer base class "unknown" is unknown. Remove the --base-class option or specify one of "oauth2" or "devise".',
-        type: 'SilentError'
-      }
-    });
+    return expect(emberNew().then(() => {
+      return generateAndDestroy(['authorizer', 'application', '--base-class=unknown'])
+    })).to.eventually.be.rejectedWith(
+      'The authorizer base class "unknown" is unknown. Remove the --base-class option or specify one of "oauth2" or "devise".'
+    );
   });
 });
