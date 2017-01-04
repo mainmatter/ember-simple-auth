257398991ccff5eb395fed0d05fc5147c5f780ff
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-04 12:49:33 +0100

    Update "ember-cli" to v2.10.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -17,6 +17,22 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-data': '~2.8.0'
+        }
+      }
+    },
+    {
       name: 'ember-lts-2.4',
       bower: {
         dependencies: {
