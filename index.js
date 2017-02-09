e37a4f5cb2fd6dabb5012ceb54551560c29d34f3
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-09 11:41:43 +0100

    Add proper this.import() polyfill
    source: https://github.com/rwjblue/ember-getowner-polyfill/blob/v1.2.2/index.js#L25-L40

--- index.js
+++ index.js
@@ -6,14 +6,27 @@
 module.exports = {
   name: 'ember-simple-auth',
 
-  included: function(app) {
+  included: function() {
     this._super.included.apply(this, arguments);
+    this._ensureThisImport();
 
-    // see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (typeof app.import !== 'function' && app.app) {
-      app = app.app;
+    this.import('vendor/ember-simple-auth/register-version.js');
+  },
+
+  _ensureThisImport: function() {
+    if (!this.import) {
+      this._findHost = function findHostShim() {
+        var current = this;
+        var app;
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+        return app;
+      };
+      this.import = function importShim(asset, options) {
+        var app = this._findHost();
+        app.import(asset, options);
+      };
     }
-
-    app.import('vendor/ember-simple-auth/register-version.js');
   }
 };
