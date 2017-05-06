a0dea324e6e97b28cbd35d70e2f93fe8bcdaace2
Steven Wu <wusteven@gmail.com>
2017-05-06 14:50:35 -0700

    Replace bower bootstrap with ember-bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,16 +12,19 @@ var sourceTrees = [];
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     storeConfigInMeta: true,
+    'ember-bootstrap': {
+      bootstrapVersion: 3,
+      importBootstrapFont: false,
+      importBootstrapCSS: true
+    }
   });
-
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 
   var yuidocTree = new Yuidoc(['addon', 'app'], {
     destDir: 'docs',
     yuidoc: {
       project: {
         name: 'The Ember Simple Auth API',
-        version: version,
+        version: version
       },
       linkNatives: false,
       quiet: true,
