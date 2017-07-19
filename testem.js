dc0bc350ac9200f694855b6cb1dc5d372f1d3422
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-07-19 14:09:26 +0200

    run tests in headless chrome (#1403)

--- testem.js
+++ testem.js
@@ -6,10 +6,20 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'PhantomJS'
+    'Chrome'
   ],
   launch_in_dev: [
-    'PhantomJS',
     'Chrome'
-  ]
+  ],
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--headless',
+        '--window-size=1440,900',
+        '--disable-gpu',
+        '--remote-debugging-port=9222'
+      ]
+    }
+  }
 };
