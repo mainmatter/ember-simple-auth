3cc47534f435608e7427c0bb245cf0d106f4fdef
Steven Wu <wusteven@gmail.com>
2017-07-24 16:26:25 +0530

    Remove trailing comma

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
-      },
+      }
     },
 
     APP: {
