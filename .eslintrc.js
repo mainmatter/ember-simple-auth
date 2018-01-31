0d99f193bff5ee3b17ca4b301cc9056dae014e98
0xADADA <0xadada.pub@0xadada.pub>
2018-01-31 10:07:08 -0500

    [#1512] Added new ember-qunit 4.3.0+ test helper
    * [x] Fixed build error by updating ESLint and configuration
    
    Closes https://github.com/simplabs/ember-simple-auth/issues/1531

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
   extends: [
     'simplabs',
     'simplabs/plugins/ember',
-  ],
+  ]
 };
