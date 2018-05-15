c0c4b82c9318f2874132ee240b9bd5e636af9c7b
Steven Wu <wusteven@gmail.com>
2018-05-15 16:57:18 -0700

    Add `Remember me` button to dummy login

--- tests/dummy/app/components/login-form.js
+++ tests/dummy/app/components/login-form.js
@@ -8,9 +8,13 @@ export default Component.extend({
   actions: {
     authenticateWithOAuth2() {
       let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-        this.set('errorMessage', reason.error);
-      });
+      this.get('session').authenticate('authenticator:oauth2', identification, password)
+        .then(() => {
+          this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
+        })
+        .catch((reason) => {
+          this.set('errorMessage', reason.error);
+        });
     },
 
     authenticateWithFacebook() {
