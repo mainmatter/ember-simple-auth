e289555354d3008e99135fdd8140dc970c82922a
Mike North <michael.l.north@gmail.com>
2018-08-23 12:15:56 -0700

    authenticated route: allow easier use w/o mixin (#1655)

--- addon/utils/is-fastboot.js
+++ addon/utils/is-fastboot.js
@@ -1,13 +1,27 @@
+/* eslint-disable no-unused-vars */
+// @ts-check
 import { computed } from '@ember/object';
+import ComputedProperty from '@ember/object/computed';
 import { getOwner } from '@ember/application';
 import { assert } from '@ember/debug';
+import ApplicationInstance from '@ember/application/instance';
 
-export default function isFastBoot() {
+/**
+ * @return {ComputedProperty<boolean>}
+ */
+export default function isFastBootCPM() {
   return computed(function() {
-    const container = getOwner(this);
-    assert('You may only use isFastBoot() on a container-aware object', container && typeof container.lookup === 'function');
-
-    const fastboot = container.lookup('service:fastboot');
-    return fastboot ? fastboot.get('isFastBoot') : false;
+    return isFastBoot(getOwner(this));
   });
+}
+
+/**
+ *
+ * @param {ApplicationInstance} owner
+ * @return {boolean}
+ */
+export function isFastBoot(owner) {
+  assert('You may only use isFastBoot() on a container-aware object', owner && typeof owner.lookup === 'function');
+  const fastboot = owner.lookup('service:fastboot');
+  return fastboot ? fastboot.get('isFastBoot') : false;
 }
