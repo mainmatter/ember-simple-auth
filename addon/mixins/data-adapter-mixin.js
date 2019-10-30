3dd4faeb0d142d72a95b3e828e894164d743a81a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-10-30 19:46:41 +0100

    Update addon/mixins/data-adapter-mixin.js
    Co-Authored-By: Samanta de Barros <samanta.dbarros@gmail.com>

--- addon/mixins/data-adapter-mixin.js
+++ addon/mixins/data-adapter-mixin.js
@@ -146,7 +146,7 @@ export default Mixin.create({
   headersForRequest() {
     deprecate('Ember Simple Auth: The headersForRequest method should no longer be used. Instead, set the headers property or implement it as a computed property.', false, {
       id: `ember-simple-auth.data-adapter-mixin.headers-for-request`,
-      until: '2.0.0'
+      until: '3.0.0'
     });
 
     const authorizer = this.get('authorizer');
