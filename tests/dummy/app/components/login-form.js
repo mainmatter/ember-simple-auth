0fd4818cad91ca31875526fadf88b50324ae57a8
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-03-17 12:16:19 +0100

    use async/await in dummy app

--- tests/dummy/app/components/login-form.js
+++ tests/dummy/app/components/login-form.js
@@ -6,15 +6,17 @@ export default Component.extend({
   session: service('session'),
 
   actions: {
-    authenticateWithOAuth2() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password)
-        .then(() => {
-          this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
-        })
-        .catch((reason) => {
-          this.set('errorMessage', reason.error);
-        });
+    async authenticateWithOAuth2() {
+      try {
+        let { identification, password } = this;
+        await this.get('session').authenticate('authenticator:oauth2', identification, password);
+
+        if (this.rememberMe) {
+          this.get('session').set('store.cookieExpirationTime', 60 * 60 * 24 * 14);
+        }
+      } catch (reason) {
+        this.set('errorMessage', reason.error);
+      }
     },
 
     authenticateWithFacebook() {
