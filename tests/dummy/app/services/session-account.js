524998e48be301cdbb60b1445726cf67c702a585
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-03-17 12:16:19 +0100

    use async/await in dummy app

--- tests/dummy/app/services/session-account.js
+++ tests/dummy/app/services/session-account.js
@@ -1,22 +1,14 @@
-import RSVP from 'rsvp';
 import Service, { inject as service } from '@ember/service';
-import { isEmpty } from '@ember/utils';
 
 export default Service.extend({
   session: service('session'),
   store: service(),
 
-  loadCurrentUser() {
-    return new RSVP.Promise((resolve, reject) => {
-      const accountId = this.get('session.data.authenticated.account_id');
-      if (!isEmpty(accountId)) {
-        this.get('store').find('account', accountId).then((account) => {
-          this.set('account', account);
-          resolve();
-        }, reject);
-      } else {
-        resolve();
-      }
-    });
+  async loadCurrentUser() {
+    let accountId = this.get('session.data.authenticated.account_id');
+    if (accountId) {
+      let account = await this.get('store').find('account', accountId);
+      this.account = account;
+    }
   }
 });
