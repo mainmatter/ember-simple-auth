b7d45a07f5e82d8e508f08a6d182b4e2b1078a46
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-03-18 11:49:30 +0100

    remove unnecessary usage of sendAction

--- tests/dummy/app/components/main-navigation.js
+++ tests/dummy/app/components/main-navigation.js
@@ -6,12 +6,6 @@ export default Component.extend({
   sessionAccount: service('session-account'),
 
   actions: {
-    login() {
-      // Closure actions are not yet available in Ember 1.12
-      // eslint-disable-next-line ember/closure-actions
-      this.sendAction('onLogin');
-    },
-
     logout() {
       this.get('session').invalidate();
     }
