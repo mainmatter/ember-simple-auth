a542801d3ed9f966334ea813a9d81f4ec60e791c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-03-18 12:45:56 +0100

    generate docs with ember-cli-yuidoc

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,6 @@
 /* eslint-disable no-var, object-shorthand */
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-var Yuidoc = require('broccoli-yuidoc');
-var version = require('git-repo-version')();
 
 var sourceTrees = [];
 
@@ -27,26 +25,6 @@ module.exports = function(defaults) {
       includePolyfill: true
     }
   });
-
-  var yuidocTree = new Yuidoc(['addon', 'app'], {
-    destDir: 'docs',
-    yuidoc: {
-      project: {
-        name: 'The Ember Simple Auth API',
-        version: version
-      },
-      linkNatives: false,
-      quiet: true,
-      parseOnly: false,
-      lint: false,
-      themedir: 'docs/theme',
-      helpers: ['docs/theme/helpers/helpers.js']
-    }
-  });
-
-  if (app.env === 'production') {
-    sourceTrees.push(yuidocTree);
-  }
 
   return app.toTree(sourceTrees);
 };
