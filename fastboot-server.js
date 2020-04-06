789133de676047219fb10c5918bcc28eb671f4f4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-04-06 17:56:34 +0200

    serve fastboot app via heroku

--- fastboot-server.js
+++ fastboot-server.js
@@ -4,6 +4,7 @@ const FastBootAppServer = require('fastboot-app-server');
 
 let server = new FastBootAppServer({
   distPath: 'dist',
+  port: process.env.PORT || 3000,
   gzip: true // Optional - Enables gzip compression.
 });
 
