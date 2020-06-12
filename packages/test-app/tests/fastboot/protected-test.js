3d17ad9a6c12688ca5dfe1cb04c40e6bd7a14ad2
Samanta de Barros <samanta.debarros@simplabs.com>
2020-06-12 17:39:04 +0200

    Mock user and posts data in fastboot test

--- packages/test-app/tests/fastboot/protected-test.js
+++ packages/test-app/tests/fastboot/protected-test.js
@@ -1,7 +1,8 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { visit } from 'ember-cli-fastboot-testing/test-support';
+import { mockServer, visit } from 'ember-cli-fastboot-testing/test-support';
 import { setupFastbootTest } from '../helpers/fastboot';
+import config from '../../config/environment';
 
 describe('FastBoot: /protected', function() {
   setupFastbootTest();
@@ -14,6 +15,20 @@ describe('FastBoot: /protected', function() {
   });
 
   it('can be accessed when the session is authenticated', async function() {
+    await mockServer.get(`${config.apiHost}/accounts/1`, {
+      data: {
+        type: 'accounts',
+        id: '1',
+        attributes: {
+          login: 'letme',
+          name: 'Some person'
+        }
+      }
+    });
+    await mockServer.get(`${config.apiHost}/posts`, {
+      data: []
+    });
+
     let { url, statusCode } = await visit('/protected', {
       headers: {
         cookie: 'ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22authenticator%22%3A%22authenticator%3Aoauth2%22%2C%22access_token%22%3A%22secret%20token!%22%2C%22account_id%22%3A1%7D%7D'
