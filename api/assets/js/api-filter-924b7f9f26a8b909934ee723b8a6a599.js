e39ce27a40a3e0b24abf6c6ee0ebb852c63f4f20
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-06-15 10:16:35 +0200

    update API docs

--- api/assets/js/api-filter-924b7f9f26a8b909934ee723b8a6a599.js
+++
@@ -1,5 +0,0 @@
-YUI.add("api-filter",function(e){e.APIFilter=e.Base.create("apiFilter",e.Base,[e.AutoCompleteBase],{initializer:function(){this._bindUIACBase(),this._syncUIACBase()},getDisplayName:function(t){return e.each(e.YUIDoc.meta.allModules,function(e){e.name===t&&e.displayName&&(t=e.displayName)}),"elements"===this.get("queryType")&&(t="&lt;"+t+"&gt;"),t}},{ATTRS:{resultHighlighter:{value:"phraseMatch"},queryType:{value:"classes"},source:{valueFn:function(){var t=this
-return function(a){var i=e.YUIDoc.meta[t.get("queryType")],s=[]
-return e.each(i,function(e){e.toLowerCase().indexOf(a.toLowerCase())>-1&&s.push(e)}),s}}}}})},"3.4.0",{requires:["autocomplete-base","autocomplete-highlighters","autocomplete-sources"]})
-
-//# sourceMappingURL=api-filter-b9b6cf5bcb8b057a700b7cbe8017e540.map
--- api/assets/js/yui-prettify-9febc570f8e801a24f3c447ce9f16a7f.js
+++
@@ -1,7 +0,0 @@
-YUI().use("node",function(a){var n=a.all(".prettyprint.linenums")
-if(n.size()){n.each(function(a){var n=a.all("ol li"),e=1
-n.each(function(a){a.prepend('<a name="LINENUM_'+e+'"></a>'),e++})})
-var e=location.hash
-location.hash="",e=e.replace("LINE_","LINENUM_"),location.hash=e}})
-
-//# sourceMappingURL=yui-prettify-2a5c42157e40bed6e05b7aa62414167c.map
