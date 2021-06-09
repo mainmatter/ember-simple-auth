e32f6156711a827a642cf6ff47bc8badbf5baed4
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2021-06-09 14:14:43 +0200

    feat: lookup and modify ApplicationRoute after EmberRouter is initialized

--- packages/ember-simple-auth/addon/initializers/setup-session-restoration.js
+++ packages/ember-simple-auth/addon/initializers/setup-session-restoration.js
@@ -1,27 +1,35 @@
 import { getOwner } from '@ember/application';
 
 export default function setupSessionRestoration(registry) {
-  const ApplicationRoute = registry.resolveRegistration
-    ? registry.resolveRegistration('route:application')
-    : registry.resolve('route:application');
+  const Router = registry.resolveRegistration ? registry.resolveRegistration('router:main') : registry.resolve('router:main');
 
-  ApplicationRoute.reopen({
-    init() {
-      this._super(...arguments);
+  Router.reopen({
+    setupRouter() {
+      const superValue = this._super(...arguments);
+      const ApplicationRoute =
+        getOwner(this).factoryFor('route:application').class;
 
-      const originalBeforeModel = this.beforeModel;
-      this.beforeModel = function() {
-        if (!this.__usesApplicationRouteMixn__) {
-          const sessionService = getOwner(this).lookup('service:session');
-          sessionService._setupHandlers();
-        }
+      ApplicationRoute.reopen({
+        init() {
+          this._super(...arguments);
 
-        const session = getOwner(this).lookup('session:main');
-        return session.restore().then(
-          () => originalBeforeModel.apply(this, arguments),
-          () => originalBeforeModel.apply(this, arguments)
-        );
-      };
+          const originalBeforeModel = this.beforeModel;
+          this.beforeModel = function() {
+            if (!this.__usesApplicationRouteMixn__) {
+              const sessionService = getOwner(this).lookup('service:session');
+              sessionService._setupHandlers();
+            }
+
+            const session = getOwner(this).lookup('session:main');
+            return session.restore().then(
+              () => originalBeforeModel.apply(this, arguments),
+              () => originalBeforeModel.apply(this, arguments)
+            );
+          };
+        },
+      });
+
+      return superValue;
     },
   });
 }
