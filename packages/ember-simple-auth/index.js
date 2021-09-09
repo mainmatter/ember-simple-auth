ecbb40430422a9c70e2e87d4941483dcbefbb13b
Bruno Casali <brunoocasali@gmail.com>
2021-09-09 08:38:18 -0300

    Remove register-version vendor file
    Closes https://github.com/simplabs/ember-simple-auth/issues/2309
    Closes https://github.com/simplabs/ember-simple-auth/issues/2303

--- packages/ember-simple-auth/index.js
+++ packages/ember-simple-auth/index.js
@@ -3,37 +3,26 @@
 /* eslint-env node */
 /* eslint-disable no-var, object-shorthand, prefer-template */
 
-var writeFile = require('broccoli-file-creator');
-var version = require('./package.json').version;
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-simple-auth',
 
-  included: function() {
+  included() {
     this._super.included.apply(this, arguments);
     this._ensureThisImport();
 
-    this.import('vendor/ember-simple-auth/register-version.js');
     this.import('vendor/base64.js');
   },
 
   treeForVendor() {
-    var content = "Ember.libraries.register('Ember Simple Auth', '" + version + "');";
-    var registerVersionTree = writeFile(
-      'ember-simple-auth/register-version.js',
-      content
-    );
-    var base64Tree = new Funnel(path.dirname(require.resolve('base-64')), {
+    return new Funnel(path.dirname(require.resolve('base-64')), {
       files: ['base64.js']
     });
-
-    return MergeTrees([registerVersionTree, base64Tree]);
   },
 
-  _ensureThisImport: function() {
+  _ensureThisImport() {
     if (!this.import) {
       this._findHost = function findHostShim() {
         var current = this;
