4f7cb619c4c083798f5f432e0f3356497041aff9
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2021-10-11 12:24:56 +0200

    Refactored data-adapter-mixin-test

--- packages/ember-simple-auth/tests/unit/mixins/data-adapter-mixin-test.js
+++ packages/ember-simple-auth/tests/unit/mixins/data-adapter-mixin-test.js
@@ -1,22 +1,22 @@
 /* eslint-disable ember/no-mixins, ember/no-new-mixins */
 
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import EmberObject from '@ember/object';
-import { describe, beforeEach, it } from 'mocha';
-import { expect } from 'chai';
 import sinonjs from 'sinon';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-describe('DataAdapterMixin', () => {
+module('DataAdapterMixin', function(hooks) {
   let sinon;
   let adapter;
   let sessionService;
   let Adapter;
 
-  beforeEach(function() {
+  hooks.beforeEach(function() {
     sinon = sinonjs.createSandbox();
     sessionService = EmberObject.create({
-      authorize() {},
-      invalidate() {}
+      authorize() { },
+      invalidate() { }
     });
 
     const BaseAdapter = EmberObject.extend({
@@ -28,52 +28,52 @@ describe('DataAdapterMixin', () => {
     adapter = Adapter.create({ session: sessionService });
   });
 
-  afterEach(function() {
+  hooks.afterEach(function() {
     sinon.restore();
   });
 
-  describe('#handleResponse', function() {
-    beforeEach(function() {
+  module('#handleResponse', function(hooks) {
+    hooks.beforeEach(function() {
       sinon.spy(sessionService, 'invalidate');
     });
 
-    describe('when the response status is 401', function() {
-      describe('when the session is authenticated', function() {
-        beforeEach(function() {
+    module('when the response status is 401', function(hooks) {
+      module('when the session is authenticated', function(hooks) {
+        hooks.beforeEach(function() {
           sessionService.set('isAuthenticated', true);
         });
 
-        it('invalidates the session', function() {
+        test('invalidates the session', function(assert) {
           adapter.handleResponse(401);
 
-          expect(sessionService.invalidate).to.have.been.calledOnce;
+          assert.ok(sessionService.invalidate.calledOnce);
         });
       });
 
-      describe('when the session is not authenticated', function() {
-        beforeEach(function() {
+      module('when the session is not authenticated', function(hooks) {
+        hooks.beforeEach(function() {
           sessionService.set('isAuthenticated', false);
         });
 
-        it('does not invalidate the session', function() {
+        test('does not invalidate the session', function(assert) {
           adapter.handleResponse(401);
 
-          expect(sessionService.invalidate).to.not.have.been.called;
+          assert.notOk(sessionService.invalidate.called);
         });
       });
     });
 
-    describe('when the response status is not 401', function() {
-      it('does not invalidate the session', function() {
+    module('when the response status is not 401', function(hooks) {
+      test('does not invalidate the session', function(assert) {
         adapter.handleResponse(200);
 
-        expect(sessionService.invalidate).to.not.have.been.called;
+        assert.notOk(sessionService.invalidate.called);
       });
     });
 
-    describe('when called via _super, and ensureResponseAuthorized is overridden', function() {
+    module('when called via _super, and ensureResponseAuthorized is overridden', function(hooks) {
       let returnValue;
-      beforeEach(function() {
+      hooks.beforeEach(function() {
         const DoesntInvalidateOn401 = Adapter.extend({
           ensureResponseAuthorized() {
             // no op, doesn't call this.get('session').invalidate();
@@ -86,17 +86,17 @@ describe('DataAdapterMixin', () => {
         returnValue = adapter.handleResponse(401);
       });
 
-      it("doesn't invalidate the session (ensureResponseAuthorized can be overridden)", function() {
-        expect(sessionService.invalidate).to.not.have.been.called;
+      test("doesn't invalidate the session (ensureResponseAuthorized can be overridden)", function(assert) {
+        assert.notOk(sessionService.invalidate.called);
       });
 
-      it("returns _super's return value", function() {
-        expect(returnValue).to.eq('_super return value');
+      test("returns _super's return value", function(assert) {
+        assert.equal(returnValue, '_super return value');
       });
     });
 
-    it("returns _super's return value", function() {
-      expect(adapter.handleResponse(401)).to.eq('_super return value');
+    test("returns _super's return value", function(assert) {
+      assert.equal(adapter.handleResponse(401), '_super return value');
     });
   });
 });
