4b057c7680914071c467b45a602d19a08118944d
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2021-10-12 09:40:10 +0200

    Refactored adaptive-test

--- packages/ember-simple-auth/tests/unit/session-stores/shared/store-behavior.js
+++ packages/ember-simple-auth/tests/unit/session-stores/shared/store-behavior.js
@@ -1,60 +1,62 @@
+import { module, test } from 'qunit';
 import { next, run } from '@ember/runloop';
-import { describe, beforeEach, afterEach, it } from 'mocha';
-import { expect } from 'chai';
 import sinonjs from 'sinon';
 
 export default function(options) {
-  let syncExternalChanges = options.syncExternalChanges || function() {};
+  let syncExternalChanges = options.syncExternalChanges || function() { };
   let sinon;
+  let hooks = options.hooks;
 
   // eslint-disable-next-line mocha/no-top-level-hooks
-  beforeEach(function() {
+  hooks.beforeEach(function() {
     sinon = sinonjs.createSandbox();
   });
 
   // eslint-disable-next-line mocha/no-top-level-hooks
-  afterEach(function() {
+  hooks.afterEach(function() {
     sinon.restore();
   });
 
-  describe('#persist', function() {
+  module('#persist', function(hooks) {
     let store;
-    beforeEach(function() {
+    hooks.beforeEach(function() {
       store = options.store(sinon, this.owner);
     });
 
-    it('persists an object', async function() {
+    test('persists an object', async function(assert) {
       await store.persist({ key: 'value' });
       let restoredContent = await store.restore();
 
-      expect(restoredContent).to.eql({ key: 'value' });
+      assert.deepEqual(restoredContent, { key: 'value' });
     });
 
-    it('overrides existing data', async function() {
+    test('overrides existing data', async function(assert) {
       await store.persist({ key: 'value' });
       await store.persist({ key1: 'value1' });
       await store.persist({ key2: 'value2' });
       let restoredContent = await store.restore();
 
-      expect(restoredContent).to.eql({ key2: 'value2' });
+      assert.deepEqual(restoredContent, { key2: 'value2' });
     });
 
-    it('does not trigger the "sessionDataUpdated" event', function(done) {
+    test('does not trigger the "sessionDataUpdated" event', async function(assert) {
       let triggered = false;
       store.one('sessionDataUpdated', () => (triggered = true));
       store.persist({ key: 'other value' });
       syncExternalChanges(store);
 
-      next(() => {
-        expect(triggered).to.be.false;
-        done();
+      await new Promise(resolve => {
+        next(() => {
+          assert.notOk(triggered);
+          resolve();
+        });
       });
     });
   });
 
-  describe('#restore', function() {
-    describe('when the store is empty', function() {
-      it('returns an empty object', async function() {
+  module('#restore', function(hooks) {
+    module('when the store is empty', function(hooks) {
+      test('returns an empty object', async function(assert) {
         let store;
         run(() => {
           store = options.store(sinon, this.owner);
@@ -62,35 +64,35 @@ export default function(options) {
         await store.clear();
         let restoredContent = await store.restore();
 
-        expect(restoredContent).to.eql({});
+        assert.deepEqual(restoredContent, {});
       });
     });
 
-    describe('when the store has data', function() {
+    module('when the store has data', function(hooks) {
       let store;
-      beforeEach(function() {
+      hooks.beforeEach(function() {
         store = options.store(sinon, this.owner);
         return store.persist({ key1: 'value1', key2: 'value2' });
       });
 
-      it('returns all data in the store', async function() {
+      test('returns all data in the store', async function(assert) {
         let restoredContent = await store.restore();
 
-        expect(restoredContent).to.eql({ key1: 'value1', key2: 'value2' });
+        assert.deepEqual(restoredContent, { key1: 'value1', key2: 'value2' });
       });
 
-      it('returns a copy of the stored data', async function() {
+      test('returns a copy of the stored data', async function(assert) {
         let data = await store.restore();
         data.key1 = 'another value!';
         let restoredContent = await store.restore();
 
-        expect(restoredContent).to.eql({ key1: 'value1', key2: 'value2' });
+        assert.deepEqual(restoredContent, { key1: 'value1', key2: 'value2' });
       });
     });
   });
 
-  describe('#clear', function() {
-    it('empties the store', async function() {
+  module('#clear', function(hooks) {
+    test('empties the store', async function(assert) {
       let store;
       run(() => {
         store = options.store(sinon, this.owner);
@@ -99,7 +101,7 @@ export default function(options) {
       await store.clear();
       let restoredContent = await store.restore();
 
-      expect(restoredContent).to.eql({});
+      assert.deepEqual(restoredContent, {});
     });
   });
 }
