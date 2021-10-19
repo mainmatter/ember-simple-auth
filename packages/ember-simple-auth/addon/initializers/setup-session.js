2cdf4ca8cc09f208a601b80dca7cfa8fe7f0d2da
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2021-10-19 08:24:38 +0200

    Removed last occurrences of ember-get-config

--- packages/ember-simple-auth/addon/initializers/setup-session.js
+++ packages/ember-simple-auth/addon/initializers/setup-session.js
@@ -1,11 +1,11 @@
-import ENV from 'ember-get-config';
+import Ember from 'ember';
 import InternalSession from '../internal-session';
 import Ephemeral from '../session-stores/ephemeral';
 
 export default function setupSession(registry) {
   registry.register('session:main', InternalSession);
 
-  if (ENV.environment === 'test') {
+  if (Ember.testing) {
     registry.register('session-store:test', Ephemeral);
   }
 }
