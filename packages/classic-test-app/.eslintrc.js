d2a93c2e232b296e5ddd77e352c63fd4159259dd
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2021-10-21 09:16:06 +0200

    Fixed CI for classic-test-app

--- packages/classic-test-app/.eslintrc.js
+++ packages/classic-test-app/.eslintrc.js
@@ -2,46 +2,65 @@
 
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   extends: [
     'simplabs',
     'simplabs/plugins/ember',
-    'plugin:ember/recommended',
+    'eslint:recommended',
   ],
+  plugins: ['ember'],
   rules: {
     'ember/local-modules': 'off',
     'ember/no-get': 'off',
     'ember/avoid-leaking-state-in-components': 'off',
   },
+  env: {
+    browser: true,
+  },
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
   },
   overrides: [
     // node files
     {
       files: [
-        'ember-cli-build.js',
         'fastboot-server.js',
-        'testem.js',
-        'config/**/*.js',
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './lib/*/index.js',
+        './server/**/*.js',
       ],
       excludedFiles: [
         'app/**',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        "node/no-extraneous-require": "off",
-        "node/no-unpublished-require": "off"
-        // add your custom rules and overrides for node files here
-      })
-    }
+      extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off',
+      },
+    },
+    {
+      // test files
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
   ]
 };
--- packages/classic-test-app/app/routes/auth-error.js
+++ packages/classic-test-app/app/routes/auth-error.js
@@ -1,8 +1,10 @@
 /* eslint-disable ember/no-mixins */
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  store: service(),
   model() {
     return this.get('store').find('post', 3);
   }
