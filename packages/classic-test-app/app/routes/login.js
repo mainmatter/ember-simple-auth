ed0e71339094f4026ec7b02af46bed061fe2bc76
Scott Newcomer <snewcomer24@gmail.com>
2021-11-08 12:28:07 -0600

    fix lint

--- packages/classic-test-app/app/routes/login.js
+++ packages/classic-test-app/app/routes/login.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-    session: service(),
+  session: service(),
 
-    beforeModel() {
-        this.session.prohibitAuthentication('index');
-    }
+  beforeModel() {
+    this.session.prohibitAuthentication('index');
+  }
 });
