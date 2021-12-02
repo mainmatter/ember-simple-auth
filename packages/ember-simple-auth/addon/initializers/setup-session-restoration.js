0b90190de791defd55420c35cd959efbe78c0614
Lou Greenwood <lougreenwood@me.com>
2021-12-02 09:25:38 +0000

    update deprecation message

--- packages/ember-simple-auth/addon/initializers/setup-session-restoration.js
+++ packages/ember-simple-auth/addon/initializers/setup-session-restoration.js
@@ -10,7 +10,9 @@ export default function setupSessionRestoration(registry) {
 
   deprecate('Ember Simple Auth: The automatic session initialization is deprecated. Please inject session service in your application route and call the setup method manually.', false, {
     id: 'ember-simple-auth.initializer.setup-session-restoration',
-    until: '5.0.0'
+    since: '4.1.0',
+    until: '5.0.0',
+    for: 'ember-simple-auth'
   });
 
   const ApplicationRoute = registry.resolveRegistration
