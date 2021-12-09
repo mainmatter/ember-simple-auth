17855cf3b7c8c4799d68ec6d8f0389d9fdcf2616
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2021-12-09 13:25:31 +0100

    removed ember-3.0 from CI because ember-qunit 5 does not support ember-3.0.

--- packages/ember-simple-auth/config/ember-try.js
+++ packages/ember-simple-auth/config/ember-try.js
@@ -15,21 +15,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-3.0',
-          bower: {
-            dependencies: {
-              ember: null,
-              'ember-data': null,
-            },
-          },
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.0.0',
-              'ember-source': '~3.0.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.4',
           bower: {
             dependencies: {
