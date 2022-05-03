492c59898a79c30f3148a8b95e04db8864c374cf
Florian Pichler <fp@ylk.gd>
2022-05-03 21:04:08 +0200

    Upgrade ESLint configuration
    * Unified configuration
    * Disable failing rules (to be undone in a separate PR)

--- packages/test-app/.eslintrc.js
+++ packages/test-app/.eslintrc.js
@@ -3,45 +3,6 @@
 module.exports = {
   root: true,
   extends: [
-    'simplabs',
-    'simplabs/plugins/ember',
-    'plugin:ember/recommended',
+    '../../.eslintrc.js',
   ],
-  rules: {
-    'ember/local-modules': 'off',
-    'ember/no-get': 'off',
-    'ember/avoid-leaking-state-in-components': 'off',
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        'ember-cli-build.js',
-        'fastboot-server.js',
-        'testem.js',
-        'config/**/*.js',
-      ],
-      excludedFiles: [
-        'app/**',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        "node/no-extraneous-require": "off",
-        "node/no-unpublished-require": "off"
-        // add your custom rules and overrides for node files here
-      })
-    }
-  ]
 };
