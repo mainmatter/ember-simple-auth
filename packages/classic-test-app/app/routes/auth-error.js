8b7839a40d0b6cacdd72fcc20820dc4020f7908d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2022-05-26 13:57:36 +0200

    inject ED store explicitly

--- packages/classic-test-app/app/routes/auth-error.js
+++ packages/classic-test-app/app/routes/auth-error.js
@@ -1,8 +1,11 @@
 /* eslint-disable ember/no-mixins */
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  store: service(),
+
   model() {
     return this.get('store').find('post', 3);
   }
