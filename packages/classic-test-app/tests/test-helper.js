503de6ff9578632c7bb79bf2e3f67cab25aac3bc
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-07-25 17:11:28 +0200

    chore: classic-test-app ember-qunit v5

--- packages/classic-test-app/tests/test-helper.js
+++ packages/classic-test-app/tests/test-helper.js
@@ -1,7 +1,11 @@
 import Application from '../app';
 import config from '../config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { setup } from 'qunit-dom';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
