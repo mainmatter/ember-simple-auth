3253367b7c4d23f63fd7ad8299d2f051a8f7fb9b
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-11-04 13:16:50 +0100

    feat: fix ember-simple-auth.initializer.setup-session-restoration deprecation

--- packages/classic-test-app/ember-cli-build.js
+++ packages/classic-test-app/ember-cli-build.js
@@ -4,7 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    // Fixes automatic setup deprecation
+    // ember-simple-auth.initializer.setup-session-restoration
+    'ember-simple-auth': {
+      useSessionSetupMethod: true,
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
