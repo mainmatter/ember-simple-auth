062b7dd471e425fb32807ddb43f5b812092c1dac
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2022-11-19 12:39:19 +0100

    feat: remove sessionSetupMethod configuration option

--- packages/ember-simple-auth/index.js
+++ packages/ember-simple-auth/index.js
@@ -15,10 +15,6 @@ module.exports = {
     this._super.included.apply(this, arguments);
     this._ensureThisImport();
 
-    const app = this._findHost();
-    const config = app.options['ember-simple-auth'] || {};
-    this.addonConfig = config;
-
     this.import('vendor/base64.js');
   },
 
@@ -26,25 +22,6 @@ module.exports = {
     return new Funnel(path.dirname(require.resolve('base-64')), {
       files: ['base64.js']
     });
-  },
-
-  treeForAddon(tree) {
-    let useSessionSetupMethodConfig = writeFile(
-      'use-session-setup-method.js',
-      `export default ${Boolean(this.addonConfig.useSessionSetupMethod)};`
-    );
-
-    return this._super.treeForAddon.call(this, MergeTrees([tree, useSessionSetupMethodConfig]));
-  },
-
-  treeForApp(tree) {
-    if (this.addonConfig.useSessionSetupMethod) {
-      return new Funnel(tree, {
-        exclude: ['routes/application.js']
-      });
-    }
-
-    return this._super.treeForApp.apply(this, arguments);
   },
 
   _ensureThisImport: function() {
