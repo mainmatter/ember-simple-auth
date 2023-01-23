3207d36525352ec853a686410309988f009c8a2e
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-01-23 15:10:07 +0100

    fix running test-app on fresh clone

--- packages/test-app/app/authenticators/torii.js
+++ packages/test-app/app/authenticators/torii.js
@@ -3,16 +3,13 @@ import Torii from 'ember-simple-auth/authenticators/torii';
 
 export default Torii.extend({
   torii: service(),
-  ajax: service(),
 
   authenticate() {
-    const ajax = this.get('ajax');
-
     return this._super(...arguments).then((data) => {
-      return ajax.request('/token', {
-        type:     'POST',
-        dataType: 'json',
-        data:     { 'grant_type': 'facebook_auth_code', 'auth_code': data.authorizationCode }
+      return fetch('/token', {
+        method:  'POST',
+        data:    JSON.stringify({ 'grant_type': 'facebook_auth_code', 'auth_code': data.authorizationCode }),
+        headers: { 'content-type': 'application/json' }
       }).then((response) => {
         return {
           access_token: response.access_token,
