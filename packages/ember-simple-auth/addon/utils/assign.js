1ad85bd68d47f4cfe6f1e69b09558bee3e982519
BobrImperator <bartholomew.dudzik@gmail.com>
2023-03-31 13:21:53 +0200

    feat: use embroider macro to conditionally import assign polyfill

--- packages/ember-simple-auth/addon/utils/assign.js
+++ packages/ember-simple-auth/addon/utils/assign.js
@@ -1,3 +1,9 @@
-import { assign } from '@ember/polyfills';
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
+
+let assign;
+
+if (macroCondition(dependencySatisfies('ember-source', '<5.x'))) {
+  assign = importSync('@ember/polyfills').assign;
+}
 
 export default Object.assign || assign;
