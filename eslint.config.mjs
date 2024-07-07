c0f65b0d24aef8e4a6b03b6317f1b4d805e3343e
bobrimperator <bartholomew.dudzik@gmail.com>
2024-07-07 13:19:02 +0200

    chore(lint): copy configs to other packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,35 @@
-import ember from "eslint-plugin-ember";
-import prettier from "eslint-plugin-prettier/recommended";
-import globals from "globals";
-import babelParser from "@babel/eslint-parser";
-import qunit from "eslint-plugin-qunit";
-import simplabsEmber from "eslint-config-simplabs";
-import n from "eslint-plugin-n";
-import js from "@eslint/js";
+import ember from 'eslint-plugin-ember';
+import prettier from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import qunit from 'eslint-plugin-qunit';
+import simplabsEmber from 'eslint-config-simplabs';
+import n from 'eslint-plugin-n';
+import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
   prettier,
   {
     ignores: [
-      "node-tests/fixtures/",
-      "blueprints/*/files/",
-      "vendor/",
-      "dist/",
-      "tmp/",
-      "bower_components/",
-      "node_modules/",
-      "coverage/",
-      "!**/.*",
-      ".node_modules.ember-try/",
-      "bower.json.ember-try",
-      "package.json.ember-try",
+      'node-tests/fixtures/',
+      'blueprints/*/files/',
+      'vendor/',
+      'dist/',
+      'tmp/',
+      'bower_components/',
+      'node_modules/',
+      'coverage/',
+      '!**/.*',
+      '.node_modules.ember-try/',
+      'bower.json.ember-try',
+      'package.json.ember-try',
     ],
   },
   {
+    files: [
+      '**/app/**/*.js'
+    ],
     plugins: {
       ember,
       simplabsEmber,
@@ -39,59 +42,56 @@ export default [
 
       parser: babelParser,
       ecmaVersion: 2020,
-      sourceType: "module",
+      sourceType: 'module',
 
       parserOptions: {
-        requireConfigFile: false,
-        ecmaFeatures: {
-          legacyDecorators: true,
-        },
+        legacyDecorators: true,
       },
     },
 
     rules: {
-      "ember/local-modules": "off",
-      "ember/no-get": "off",
-      "ember/avoid-leaking-state-in-components": "off",
+      'ember/local-modules': 'off',
+      'ember/no-get': 'off',
+      'ember/avoid-leaking-state-in-components': 'off',
 
       // Legacy Ember features
-      "ember/no-actions-hash": "off",
-      "ember/no-classic-classes": "off",
-      "ember/no-classic-components": "off",
-      "ember/no-private-routing-service": "off",
-      "ember/require-tagless-components": "off",
+      'ember/no-actions-hash': 'off',
+      'ember/no-classic-classes': 'off',
+      'ember/no-classic-components': 'off',
+      'ember/no-private-routing-service': 'off',
+      'ember/require-tagless-components': 'off',
 
       // TODO: REMOVE
-      "no-implicit-coercion": "off",
-      "no-multi-spaces": "off",
-      "no-multi-str": "off",
-      "no-trailing-spaces": "off",
-      "no-unused-vars": "off",
-      "no-var": "off",
-      "prefer-template": "off",
+      'no-implicit-coercion': 'off',
+      'no-multi-spaces': 'off',
+      'no-multi-str': 'off',
+      'no-trailing-spaces': 'off',
+      'no-unused-vars': 'off',
+      'no-var': 'off',
+      'prefer-template': 'off',
       quotes: 'off',
-      semi: "off",
+      semi: 'off',
     },
   },
   {
     files: [
-      "**/.babelrc.js",
-      "**/.eslintrc.js",
-      "**/.eslintrc.js",
-      "**/eslint.config.mjs",
-      "**/prettier.config.mjs",
-      "**/addon-main.js",
+      '**/.babelrc.js',
+      '**/.eslintrc.js',
+      '**/.eslintrc.js',
+      '**/eslint.config.mjs',
+      '**/prettier.config.mjs',
+      '**/addon-main.js',
       '**/server/**/*.js',
       '**/server/*.js',
-      "**/blueprints/*/index.js",
-      "**/config/**/*.js",
-      "**/config/*.js",
+      '**/blueprints/*/index.js',
+      '**/config/**/*.js',
+      '**/config/*.js',
       '**/lib/my-engine/index.js',
       '**/testem.js',
-      '**/ember-cli-build.js'
+      '**/ember-cli-build.js',
     ],
 
-    ignores: ["src/**"],
+    ignores: ['src/**'],
 
     plugins: {
       n,
@@ -103,20 +103,20 @@ export default [
       },
 
       ecmaVersion: 6,
-      sourceType: "script",
+      sourceType: 'script',
     },
     rules: {
-      "node/no-extraneous-require": "off",
-      "node/no-unpublished-require": "off",
+      'node/no-extraneous-require': 'off',
+      'node/no-unpublished-require': 'off',
     },
   },
   {
     plugins: {
       qunit,
     },
-    files: ["**/tests/**/*-test.js"],
+    files: ['**/tests/**/*-test.js'],
     rules: {
-      "qunit/require-expect": ["error", "except-simple"],
+      'qunit/require-expect': ['error', 'except-simple'],
       quotes: ['error', 'single'],
     },
   },
---
+++ packages/test-esa/prettier.config.mjs
@@ -0,0 +1,1 @@
+export { default } from '../../prettier.config.mjs';
