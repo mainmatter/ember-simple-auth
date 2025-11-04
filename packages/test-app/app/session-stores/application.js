c5067cdc781a40fbe8a71209cdf87b490bc02ce9
BobrImperator <bartholomew.dudzik@gmail.com>
2025-11-04 18:24:04 +0100

    chore: build test-app with correct session-store based on FASTBOOT_DISABLED env

--- packages/test-app/app/session-stores/application.js
+++ packages/test-app/app/session-stores/application.js
@@ -1,30 +1,28 @@
-import { isTesting, macroCondition, importSync } from '@embroider/macros';
+import { macroCondition, getOwnConfig } from '@embroider/macros';
 import CookieStore from 'ember-simple-auth/session-stores/cookie';
 import AdaptiveStore from 'ember-simple-auth/session-stores/adaptive';
 
-let klass = class extends AdaptiveStore {
-  init(emberOwner: any): void {
-    let __isLocalStorageAvailable = determineStorageBackend();
-    super.init(emberOwner, __isLocalStorageAvailable);
-  }
-};
-if (macroCondition(globalThis.FastBoot)) {
+let klass = class extends CookieStore {};
+if (macroCondition(getOwnConfig().FASTBOOT_DISABLED)) {
   // Playwright testing
-  klass = class extends CookieStore { };
-}
+  klass = class extends AdaptiveStore {
+    init(emberOwner) {
+      let __isLocalStorageAvailable = determineStorageBackend();
+      super.init(emberOwner, __isLocalStorageAvailable);
+    }
+  };
+  function determineStorageBackend() {
+    const storageBackend = globalThis.ESA_STORAGE_BACKEND;
 
-console.log(klass);
-export default klass;
+    if (storageBackend === 'localStorage') {
+      return true;
+    } else if (storageBackend === 'cookieStorage') {
+      return false;
+    }
 
-function determineStorageBackend(): boolean | null {
-  const storageBackend = globalThis.ESA_STORAGE_BACKEND;
-
-  if (storageBackend === 'localStorage') {
-    return true;
-  } else if (storageBackend === 'cookieStorage') {
-    return false;
+    // Adaptive
+    return null;
   }
-
-  // Adaptive
-  return null;
 }
+
+export default klass;
